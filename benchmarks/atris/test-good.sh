983264cc7c21a9c8ce3171f6ba8e57b855985afd
Chris Timperley <christimperley@googlemail.com>
2015-09-28 18:42:29 -0400

    Finished prototype of test script for atris.

--- benchmarks/atris/test-good.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-ulimit -t 2
-rm -rf my.help my.normal 
-export HOME="."
-export DISPLAY=""
-($1 -h >& my.help ; diff output.help my.help && (echo "t1" >> $2)) &
-($1 >& my.normal ; diff output.normal my.normal && (echo "t2" >> $2)) &
-wait
-exit 0
