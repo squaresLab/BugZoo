165c993403d8a5542a01a430f20e2f80a37928ef
Chris Timperley <christimperley@googlemail.com>
2015-09-28 20:31:05 -0400

    Modified test script for flex.

--- benchmarks/flex/test.sh
+++ benchmarks/flex/test.sh
@@ -18,16 +18,18 @@ setup_neg()
 
 # Execute the test case with the given ID.
 case $2 in
-  p1) setup_pos ; $1 < t1 |& diff $DIR/test/output.t1 - && exit 0 ;;
-  p2) setup_pos ; $1 < t2 |& diff $DIR/test/output.t2 - && exit 0 ;;
-  p3) setup_pos ; $1 < t3 |& diff $DIR/test/output.t3 - && exit 0 ;;
-  p4) setup_pos ; $1 < t4 |& diff $DIR/test/output.t4 - && exit 0 ;;
-  p5) setup_pos ; $1 < t5 |& diff $DIR/test/output.t5 - && exit 0 ;;
+  p1) setup_pos ; bash -c "$1 < $DIR/test/t1" |& diff $DIR/test/output.t1 - && exit 0 ;;
+  p2) setup_pos ; bash -c "$1 < $DIR/test/t2" |& diff $DIR/test/output.t2 - && exit 0 ;;
+  p3) setup_pos ; bash -c "$1 < $DIR/test/t3" |& diff $DIR/test/output.t3 - && exit 0 ;;
+  p4) setup_pos ; bash -c "$1 < $DIR/test/t4" |& diff $DIR/test/output.t4 - && exit 0 ;;
+  p5) setup_pos ; bash -c "$1 < $DIR/test/t5" |& diff $DIR/test/output.t5 - && exit 0 ;;
 
   n1)
     setup_neg
-    rm -rf my.t11 core.*
-    ($1 < $DIR/test/t11 &> my.t11)
-    if [ ! -f core.* ] ; then exit 0; fi 
+    rm -rf core.*
+    bash -c "$1 < $DIR/test/t11"
+    if [ ! -f core.* ] ; then
+      exit 0
+    fi
 esac 
 exit 1
