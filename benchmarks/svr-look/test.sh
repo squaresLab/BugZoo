ecf28218e5c062ef38303b1cac7aa0d11f46c13e
Chris Timperley <christimperley@googlemail.com>
2015-10-06 23:44:09 -0400

    Tidied up svr-look benchmark.

---
+++ benchmarks/svr-look/test.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Retrieve and store the provided command-line arguments.
+EXECUTABLE=$1
+TEST_ID=$2
+
+# Find the directory that this test script belongs to.
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+# Check if this test script is being used to compute coverage information.
+if [ $(basename $1) = "coverage" ]; then
+  cov=1
+else
+  cov=0
+fi
+
+# Treats the test case as a positive test case.
+exec_pos()
+{
+  if [ $cov = 0 ]; then
+    timeout 1 $EXECUTABLE $DIR/test/input $DIR/test/$TEST_ID &> /dev/null
+  else
+    $EXECUTABLE $DIR/test/input $DIR/test/$TEST_ID &> /dev/null
+  fi
+  return $?
+}
+
+# Treats the test case as a negative test case.
+exec_neg()
+{
+  if [ $cov = 0 ]; then
+    timeout 1 $EXECUTABLE $DIR/test/input $DIR/test/input &> /dev/null
+  else
+    $EXECUTABLE $DIR/test/input $DIR/test/$TEST_ID &> /dev/null
+  fi
+  return $?
+}
+
+# Execute the test case with the given ID.
+case $TEST_ID in
+  p1) exec_pos;;
+  p2) exec_pos;;
+  p3) exec_pos;;
+  p4) exec_pos;;
+#  p5) exec_pos;;
+  n1) exec_neg;;
+esac
+
+# Find the result of the test case execution.
+result=$?
+exit $result
