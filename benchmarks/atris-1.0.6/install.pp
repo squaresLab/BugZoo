c65ed0b3c2256a7c15bf698dfd292d62c9bc1cc3
ChrisTimperley <christimperley@googlemail.com>
2016-02-14 02:32:21 +0000

    Added installation file for atris benchmark.

---
+++ benchmarks/atris-1.0.6/install.pp
@@ -0,0 +1,5 @@
+package { 'libsdl1.2debian': ensure => present }
+package { 'libsdl1.2-dev': ensure => present }
+package { 'libsdl-ttf2.0-0': ensure => present }
+package { 'libsdl-ttf2.0-dev': ensure => present }
+package { 'libsdl-ttf-gst': ensure => present }
