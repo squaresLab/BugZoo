e4b4a01807772b8e62a5764d584917d343dd0bac
ChrisTimperley <christimperley@googlemail.com>
2016-02-15 02:13:53 +0000

    Begun adding ocaml + opam to Puppet file.

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -2,9 +2,44 @@ exec { "apt-get update":
   path => "/usr/bin"
 }
 
-package { "git":
-  ensure => present,
+package { "python-software-properties":
+  ensure  => present,
   require => Exec["apt-get update"]
 }
 
-package { "lefence": ensure => present }
+package { "make":
+  ensure  => present,
+  require => Exec["apt-get update"]
+}
+
+package { "git":
+  ensure  => present,
+  require => Exec["apt-get update"]
+}
+
+# OCaml dependecies
+package { "m4":
+  ensure  => present,
+  require => Exec["apt-get update"]
+}
+
+# Install ocaml-4.02.1 and opam-1.2.2
+exec { "ppa:ocaml+opam":
+  command =>"add-apt-repository --yes ppa:avsm/ocaml42+opam120; apt-get update",
+  path    => "/usr/bin",
+  require => Package["python-software-properties"]
+}
+
+#exec { "ocaml":
+#  command => "apt-get install ocaml --force-yes -y",
+#  path    => "/usr/bin",
+#  require => Exec["ppa:ocaml+opam"]
+#}
+
+#exec { "opam":
+#  command => "apt-get install opam --force-yes -y",
+#  path    => "/usr/bin",
+#  require => Exec["ocaml"]
+#}
+
+# Perform initial opam config
