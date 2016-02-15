2dc078a3a816aa110c1ff35bbae8e0ddf3153106
ChrisTimperley <christimperley@googlemail.com>
2016-02-15 17:16:46 +0000

    Almost finished integration of ocaml + opam into Puppet file.

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -1,6 +1,9 @@
-exec { "apt-get update":
-  path => "/usr/bin"
+# set default execution path
+Exec {
+  path => ['/bin/', '/usr/bin/']
 }
+
+exec { "apt-get update": }
 
 package { "python-software-properties":
   ensure  => present,
@@ -17,29 +20,41 @@ package { "git":
   require => Exec["apt-get update"]
 }
 
-# OCaml dependecies
 package { "m4":
   ensure  => present,
   require => Exec["apt-get update"]
 }
 
-# Install ocaml-4.02.1 and opam-1.2.2
 exec { "ppa:ocaml+opam":
   command =>"add-apt-repository --yes ppa:avsm/ocaml42+opam120; apt-get update",
-  path    => "/usr/bin",
   require => Package["python-software-properties"]
 }
 
-#exec { "ocaml":
-#  command => "apt-get install ocaml --force-yes -y",
-#  path    => "/usr/bin",
-#  require => Exec["ppa:ocaml+opam"]
-#}
+# install ocaml-4.02.1
+exec { "ocaml":
+  require => [Exec["ppa:ocaml+opam"], Package["m4"]],
+  command => "apt-get install -y --force-yes ocaml"
+}
 
-#exec { "opam":
-#  command => "apt-get install opam --force-yes -y",
-#  path    => "/usr/bin",
-#  require => Exec["ocaml"]
-#}
+# install and initialise OPAM 1.2.2, before updating package listings
+exec { "opam":
+  require => Exec["ocaml"],
+  command => "apt-get install -y --force-yes opam; opam init -y; echo 'eval $(opam config env)' >> ~/.profile; source ~/.profile"
+}
+exec { "opam update": require => Exec["opam"] }
 
-# Perform initial opam config
+# install OPAM packages
+exec { "ocamlfind":
+  require => Exec["opam update"],
+  command => "opam install -y ocamlfind"
+}
+exec { "yojson":
+  path    => "/usr/bin",
+  command => "opam install -y yojson",
+  require => Exec["opam update"]
+}
+exec { "cil":
+  path    => "/usr/bin",
+  command => "opam install -y cil",
+  require => [Exec["ocamlfind"], Exec["opam update"]]
+}
