f87c310fd279f578adb3e9cb2d51b944ae79c9da
ChrisTimperley <christimperley@googlemail.com>
2016-03-24 14:36:13 +0000

    Rewriting ccrypt tests.

--- benchmarks/ccrypt-1.2/test.sh
+++ benchmarks/ccrypt-1.2/test.sh
@@ -10,38 +10,35 @@ TEST_ID=$2
 # Find the directory that this test script belongs to.
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
+# Determine whether coverage is being computed, and from that choose an
+# appropriate timeout length
+if [ $(basename $EXECUTABLE) = "coverage" ]; then
+  TIMEOUT=10
+else
+  TIMEOUT=1
+fi
+
 # Perform necessary preparations before running the test case.
 ulimit -c 8
-ulimit -t 10
-
-# Assume that the test case fails, until proven otherwise.
-result=1
 
 # Execute the test case with the given ID.
 case $TEST_ID in
 
   # test that the help function works correctly.
   p1)
-    timeout 10 bash -c "$EXECUTABLE -help" \
+    timeout $TIMEOUT bash -c "$EXECUTABLE -help" \
       |& diff $DIR/test/p1.out - &> /dev/null
     result=$?;;
 
-  # test that a given input can be encrypted using the KEY key.
-  # only checks that the program terminates without failure.
+  # normal encryption
   p2)
     cp $DIR/test/p2.in p2.txt
-    timeout 10 bash -c "$EXECUTABLE -e -E KEY p2.txt && diff p2.txt.cpt $DIR/test/p2.out" &> /dev/null
+    timeout $TIMEOUT bash -c " \
+      $EXECUTABLE -e -E KEY p2.txt && \
+      $EXECUTABLE -d -E KEY p2.txt && \
+      diff p2.txt $DIR/test/p2.in"
     result=$?
-    rm -rf p2.txt p2.txt.cpt;;
-
-  # decrypt the expected result from the previous test and check that it
-  # matches the plaintext input used.
-  p3)
-    cp $DIR/test/p2.out p3.txt.cpt
-    timeout 10 bash -c "$EXECUTABLE -d -E KEY p3.txt" \
-      && diff p3.txt $DIR/test/p2.in
-    result=$?
-    rm -rf p3.txt p3.txt.cpt;;
+    rm -f p2.txt p2.txt.cpt;; 
 
   # attempt to forcibly encrypt the same input from test 2, whilst there is an
   # encrypted file with a matching name (plus a .cpt suffix) in the same
@@ -55,8 +52,10 @@ case $TEST_ID in
     echo "yes\n" > yes.txt
     exec 3<> yes.txt
 
-    timeout 10 bash -c "$EXECUTABLE -e -E KEY p4.txt <&3" \
-      && diff $DIR/test/p5.out p4.txt.cpt
+    timeout $TIMEOUT bash -c " \
+      $EXECUTABLE -e -E KEY p4.txt <&3 && \
+      $EXECUTABLE -d -E KEY p4.txt && \
+      diff p4.txt $DIR/test/p2.in"
     result=$?
 
     # remove yes.txt from file descriptor 3, and destroy all temporary files.
@@ -72,32 +71,24 @@ case $TEST_ID in
     echo "\n\n" > no.txt
     exec 4<> no.txt
 
-    cp $DIR/test/p2.in p6.txt
-    cp $DIR/test/placeholder.cpt p6.txt.cpt
+    cp $DIR/test/p2.in p5.txt
+    cp $DIR/test/placeholder.cpt p5.txt.cpt
 
-    timeout 10 bash -c "$EXECUTABLE -e -E KEY p6.txt <& 4" \
-      && diff $DIR/test/p2.in p6.txt \
-      && diff $DIR/test/placeholder.cpt p6.txt.cpt
+    timeout $TIMEOUT bash -c " \
+      $EXECUTABLE -e -E KEY p5.txt <&4 && \
+      diff p5.txt $DIR/test/p2.in && \
+      diff p5.txt.cp $DIR/test/placeholder.cpt"
     result=$?
 
     exec 4>&-
     rm -f no.txt p6.txt p6.txt.cpt;;
 
-  # This test case simply checks that a given decryption results in the
-  # expected plaintext.
-  p6)
-    cp $DIR/test/p7.in p7.txt.cpt
-    timeout 10 bash -c "$EXECUTABLE -d -E KEY p7.txt.cpt" \
-      && diff $DIR/test/p7.out p7.txt
-    result=$?
-    rm -f p7.txt p7.txt.cpt;;
-
-  # checks that the program terminates without error when supplied with the
-  # negative plaintext and when /dev/null is fed to the program
+  # the program should terminate when fed /dev/null
   n1)
     cp $DIR/test/n1.in n1.txt
     cp $DIR/test/n1.cpt n1.txt.cpt
-    timeout 5 bash -c "$EXECUTABLE -e -E KEY n1.txt < /dev/null" \
+
+    timeout $TIMEOUT bash -c "$EXECUTABLE -e -E KEY n1.txt < /dev/null" \
       && diff $DIR/test/n1.in n1.txt \
       && diff $DIR/test/n1.cpt n1.txt.cpt
     result=$?
@@ -106,7 +97,7 @@ case $TEST_ID in
       result=0
     fi
     rm -rf core.*
-    rm -rf n1.txt n1.txt.cpt;;
+    rm -f n1.txt n1.txt.cpt;;
 esac
 
 # Return the result of the test case execution
