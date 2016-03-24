ec2d2bb692a3dc3db1f515f7b6f138726b4334b6
ChrisTimperley <christimperley@googlemail.com>
2016-03-24 16:56:59 +0000

    Begun rewriting deroff-ultrix-4.3 tests

--- benchmarks/deroff-ultrix-4.3/test.sh
+++ benchmarks/deroff-ultrix-4.3/test.sh
@@ -5,23 +5,34 @@ TEST_ID=$2
 # Find the directory that this test script belongs to.
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
+# Determine whether coverage is being computed, and from that choose an
+# appropriate timeout length
+if [ $(basename $EXECUTABLE) = "coverage" ]; then
+  TIMEOUT=10
+else
+  TIMEOUT=1
+fi
+
+# Perform necessary preparations before running the test case.
+ulimit -c 8
+
 positive()
 {
   case=$1
-  if [ $(basename $EXECUTABLE) = "coverage" ]; then
-    $EXECUTABLE < $DIR/test/$case |& diff $DIR/test/output.$case -
-  else
-    timeout 4 $EXECUTABLE < $DIR/test/$case |& diff $DIR/test/output.$case -
-  fi
+  timeout $TIMEOUT $EXECUTABLE < $DIR/test/$case.in &> result.$case.tmp \
+    && diff $DIR/test/$case.out result.$case.tmp
+  result=$?
+  rm -f result.$case.tmp
+  rm -f core.*
+  exit $result
 }
 
 negative()
 {
-  if [ $(basename $EXECUTABLE) = "coverage" ]; then
-    $EXECUTABLE < $DIR/test/t5
-  else
-    timeout 4 $EXECUTABLE < $DIR/test/t5
-  fi
+  timeout $TIMEOUT $EXECUTABLE < $DIR/test/n1.in
+  result=$?
+  rm -f core.*
+  exit $result
 }
 
 case $TEST_ID in
