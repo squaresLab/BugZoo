fbbc5c011ffc620abbac53201fc4ef1b82a9d4fb
Chris Timperley <christimperley@googlemail.com>
2016-05-05 23:41:35 +0100

    Tidied up Fedora provisioning script.

--- shell/fedora22.sh
+++ shell/fedora22.sh
@@ -15,15 +15,7 @@ sudo dnf install -y perl-CPAN
 
 # Install ocaml and opam
 sudo dnf install -y ocaml
-#wget http://download.opensuse.org/repositories/home:ocaml/Fedora_22/home:ocaml.repo \
-#  -O /etc/yum.repos.d/home:ocaml.repo
-#sudo yum install -y opam
-
-#sudo yum copr enable jonludlam/opam
-#sudo yum copr enable cottsay/gringo
-#yum install -y aspcud
-
-if ! [ -f /usr/local/bin/opam]; then
+if ! [ -f /usr/local/bin/opam ] ; then
   sudo wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh \
     -O - | sh -s /usr/local/bin
   opam update
@@ -37,30 +29,9 @@ opam install -y cil
 #sudo dnf install -y libgmp3-dev
 #sudo dnf install -y m4 libglib2.0-dev libgmp3-dev
 
-#exec { "opam config":
-#  require => [Exec["opam init"], User['vagrant']],
-#  command => "echo 'eval $(opam config env)' >> /home/vagrant/.profile && touch /home/vagrant/.opamrc && . /home/vagrant/.profile",
-#  unless => "bash -c 'test -f /home/vagrant/.opamrc'",
-#  logoutput => on_failure,
-#  user => "vagrant"
-#}
-## install OPAM packages
-#exec { "ocamlfind":
-#  require => [Exec["opam update"], User['vagrant']],
-#  command => "opam install -y ocamlfind --root=/home/vagrant/.opam",
-#  user => "vagrant"
-#}
-#exec { "yojson":
-#  command => "opam install -y yojson --root=/home/vagrant/.opam",
-#  require => [Exec["opam update"], User['vagrant']],
-#  user => "vagrant"
-#}
-#exec { "cil":
-#  command => "opam install -y cil --root=/home/vagrant/.opam",
-#  require => [Exec["ocamlfind"], Exec["opam update"], User['vagrant']],
-#  user => "vagrant"
-#}
-#
+# Atris requirements
+#opam install -y SDL*
+
 ## Required by various benchmarks
 ## Atris
 #package { 'libsdl1.2debian': require => Package['make'] }
