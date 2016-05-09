42fab609b381641acbb6576aabb52fd14ffc9901
Chris Timperley <christimperley@googlemail.com>
2016-05-09 02:24:52 +0000

    Updated python-bug-69368-69372

--- python-bug-69368-69372/prepare.sh
+++ python-bug-69368-69372/prepare.sh
@@ -6,3 +6,7 @@ if [ ! -d python ]; then
   ./configure --build=i686-pc-linux-gnu "CFLAGS=-m32 -std=gnu99 -fgnu89-inline" "CXXFLAGS=-m32" "LDFLAGS=-m32"
   popd
 fi
+
+if [ ! -d test ]; then
+  tar -xf tests.tar.gz
+fi
