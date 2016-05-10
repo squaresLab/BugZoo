c1c8bbbb374c1351c6af4511fd0a5e5b76714114
Chris Timperley <christimperley@googlemail.com>
2016-05-10 02:10:41 +0000

    Modified gzip benchmark.

--- gzip/gzip-bug-2009-08-16-3fe0caeada-39a362ae9d/test.sh
+++ gzip/gzip-bug-2009-08-16-3fe0caeada-39a362ae9d/test.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
-bugrev=3fe0caeada
+EXECUTABLE=$( dirname $1 )
+TEST_ID=$2
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+#Check if coverage is being run. If so, don't use time limit.
+if [ `basename $1` = "coverage" ] ; then
+  TIMEOUT=120
+else
+  TIMEOUT=60
+fi
 
 run_test()
 {
-    cd gzip
-        /root/mountpoint-genprog/genprog-many-bugs/gzip-bug-2009-08-16-3fe0caeada-39a362ae9d/limit /usr/bin/perl /root/mountpoint-genprog/genprog-many-bugs/gzip-bug-2009-08-16-3fe0caeada-39a362ae9d/gzip-run-tests.pl $1
+    pushd gzip
+    timeout $TIMEOUT $DIR/php-run-tests $1
     RESULT=$?
-    
-    cd ..
+    popd
     return $RESULT
 }
-case $1 in
+
+case $TEST_ID in
     p1) run_test 1 && exit 0 ;; 
     p2) run_test 4 && exit 0 ;; 
     n1) run_test 3 && exit 0 ;; 
