ebb2aeca1fc5a3eea3727b0da2d96f7ed01a6961
Chris Timperley <christimperley@googlemail.com>
2016-05-10 19:35:17 +0000

    Added lighttpd-bug-1806-1807

---
+++ many-bugs/lighttpd/lighttpd-bug-1806-1807/test.sh
@@ -0,0 +1,19 @@
+    p1) run_test 2 && exit 0 ;; 
+    p2) run_test 3 && exit 0 ;; 
+    p3) run_test 7 && exit 0 ;; 
+    p4) run_test 12 && exit 0 ;; 
+    p5) run_test 15 && exit 0 ;; 
+    n1) run_test 1 && exit 0 ;; 
+    n2) run_test 4 && exit 0 ;; 
+    n3) run_test 6 && exit 0 ;; 
+    n4) run_test 8 && exit 0 ;; 
+    n5) run_test 10 && exit 0 ;; 
+    n6) run_test 11 && exit 0 ;; 
+    n7) run_test 14 && exit 0 ;; 
+    n8) run_test 16 && exit 0 ;; 
+    n9) run_test 17 && exit 0 ;; 
+    n10) run_test 18 && exit 0 ;; 
+    n11) run_test 20 && exit 0 ;; 
+    n12) run_test 21 && exit 0 ;; 
+esac
+exit 1
