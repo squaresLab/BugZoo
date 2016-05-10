6a187273e5f2b3eb6f4081dbc845fbb3c11c3449
Chris Timperley <christimperley@googlemail.com>
2016-05-10 01:35:46 +0000

    Added installation to local root into PHP prepare script.

--- php/prepare.sh
+++ php/prepare.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+INSTALL_TO="$DIR/local-root"
+
 if [ ! -d php ]; then
+  mkdir -p local-root
   tar -xf php.tar.gz
   cp libxml.patch php
   pushd php
   cat libxml.patch | patch -p0
   rm libxml.patch
-  ./configure --build=i686-pc-linux-gnu "CFLAGS=-m32" "CXXFLAGS=-m32" "LDFLAGS=-m32"
+  ./configure --prefix=$INSTALL_TO --build=i686-pc-linux-gnu "CFLAGS=-m32" "CXXFLAGS=-m32" "LDFLAGS=-m32"
   popd
 fi
