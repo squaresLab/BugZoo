9097cf8c38d37e463d71c6b9b91df5173c6f9168
Chris Timperley <christimperley@googlemail.com>
2016-05-10 20:09:36 +0000

    Fixed lighttpd test scripts

--- many-bugs/lighttpd/lighttpd-bug-1806-1807/test.sh
+++ many-bugs/lighttpd/lighttpd-bug-1806-1807/test.sh
@@ -1,3 +1,25 @@
+#!/bin/bash
+EXECUTABLE=$( dirname $1 )
+TEST_ID=$2
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+#Check if coverage is being run. If so, don't use time limit.
+if [ `basename $1` = "coverage" ] ; then
+  TIMEOUT=120
+else
+  TIMEOUT=60
+fi
+
+run_test()
+{
+    pushd lighttpd
+    timeout $TIMEOUT $DIR/test.pl $1
+    RESULT=$?
+    popd
+    return $RESULT
+}
+
+case $TEST_ID in
     p1) run_test 2 && exit 0 ;; 
     p2) run_test 3 && exit 0 ;; 
     p3) run_test 7 && exit 0 ;; 
