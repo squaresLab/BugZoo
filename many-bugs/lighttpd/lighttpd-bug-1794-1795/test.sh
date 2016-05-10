f3efb353c4cde46992cd8a789c8a8d3a81bd580c
Chris Timperley <christimperley@googlemail.com>
2016-05-10 16:29:15 +0000

    Added lighttpd-bug-1794-1795 benchmark.

---
+++ many-bugs/lighttpd/lighttpd-bug-1794-1795/test.sh
@@ -0,0 +1,7 @@
+    p1) run_test 2 && exit 0 ;; 
+    p2) run_test 3 && exit 0 ;; 
+    p3) run_test 7 && exit 0 ;; 
+    p4) run_test 15 && exit 0 ;; 
+    n1) run_test 12 && exit 0 ;; 
+esac
+exit 1
