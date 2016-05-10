9b2afe1e4b2ccbcab13e648d53ba69f168f8f3dd
Chris Timperley <christimperley@googlemail.com>
2016-05-10 13:06:37 +0000

    sanity checked gzip-bug-2010-02-19-3eb6091d69-884ef6d16c

--- gzip/gzip-bug-2010-02-19-3eb6091d69-884ef6d16c/test.sh
+++ gzip/gzip-bug-2010-02-19-3eb6091d69-884ef6d16c/test.sh
@@ -1,29 +1,25 @@
 #!/bin/bash
-bugrev=3eb6091d69
+EXECUTABLE=$( dirname $1 )
+TEST_ID=$2
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 #Check if coverage is being run. If so, don't use time limit.
-if [ `basename $2` = "coverage" ] ; then
-    cov=0
+if [ `basename $1` = "coverage" ] ; then
+  TIMEOUT=120
 else
-    cov=1
+  TIMEOUT=60
 fi
 
 run_test()
 {
-    cd gzip
-    if [ $cov -eq 0 ] ; then
-        /usr/bin/perl /root/mountpoint-genprog/genprog-many-bugs/gzip-bug-2010-02-19-3eb6091d69-884ef6d16c/gzip-run-tests.pl $1
-    else
-        /root/mountpoint-genprog/genprog-many-bugs/gzip-bug-2010-02-19-3eb6091d69-884ef6d16c/limit /usr/bin/perl /root/mountpoint-genprog/genprog-many-bugs/gzip-bug-2010-02-19-3eb6091d69-884ef6d16c/gzip-run-tests.pl $1
-    fi
+    pushd gzip
+    timeout $TIMEOUT $DIR/test.pl $1
     RESULT=$?
-    
-    cd ..
+    popd
     return $RESULT
 }
-case $1 in
-    p1) run_test 1 && exit 0 ;; 
-    p2) run_test 3 && exit 0 ;; 
+
+case $TEST_ID in
     p3) run_test 4 && exit 0 ;; 
     p4) run_test 5 && exit 0 ;; 
     p5) run_test 7 && exit 0 ;; 
