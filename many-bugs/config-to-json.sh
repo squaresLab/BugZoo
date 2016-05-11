6c707b4c951d27f5c4812c0bb333af68591ccd38
Chris Timperley <christimperley@googlemail.com>
2016-05-11 16:03:50 +0000

    Added test case extraction to config conversion script

--- many-bugs/config-to-json.sh
+++ many-bugs/config-to-json.sh
@@ -12,3 +12,15 @@ cfg_dir=$(dirname $1)
 benchmark=$(basename $cfg_dir)
 
 echo "Converting configuration-default in $cfg_dir...";
+
+# Find the program used by the benchmark
+_parts=(${benchmark//-/ })
+program=${_parts[0]}
+
+# Find number of positive and negative test cases
+pos_tests=$(grep "pos-tests" $cfg | cut -d " " -f2 )
+neg_tests=$(grep "neg-tests" $cfg | cut -d " " -f2 )
+
+echo "  program: $program"
+echo "  positive_tests: $pos_tests"
+echo "  negative_tests: $neg_tests"
