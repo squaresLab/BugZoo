79489c5eee8132aad59be53f12726d6512a26c48
Chris Timperley <christimperley@googlemail.com>
2016-05-11 12:07:27 +0000

    Updated GMP prepare with aclocal and autoconf

--- many-bugs/gmp/gmp-bug-14166-14167/prepare.sh
+++ many-bugs/gmp/gmp-bug-14166-14167/prepare.sh
@@ -3,6 +3,8 @@ if [ ! -d gmp ]; then
   tar -xf gmp.tar.gz
   pushd gmp
   make clean
+  aclocal
+  autoconf
   ./configure --build=i686-pc-linux-gnu "CFLAGS=-m32" "CXXFLAGS=-m32" "LDFLAGS=-m32"
   popd
 fi
