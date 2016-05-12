0cbf0785537360898bc717c7e47afa83a5ad7d92
Chris Timperley <christimperley@googlemail.com>
2016-05-12 14:47:54 +0000

    Begun PHP test harness conversion script

---
+++ many-bugs/php/build-test-harness.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+# This script accepts a provided PHP benchmark directory path as its sole input
+# and proceeds to destructively convert its C test harness into a perl script,
+# matching the rest of the ManyBugs test harnesses.
+#
+# Usage: ./build-test-harness.sh PHP_BENCHMARK_DIR
