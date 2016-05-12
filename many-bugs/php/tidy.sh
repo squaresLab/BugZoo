53e970a75cb3502bd2f723c956f8e57529ee50de
Chris Timperley <christimperley@googlemail.com>
2016-05-12 15:08:06 +0000

    Added tidy script specific to PHP

---
+++ many-bugs/php/tidy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# This script is responsible for performing benchmark directory tidying
+# specific to PHP scenarios. Like other tidy scripts, it accepts the path to a
+# benchmark directory, and then proceeds to performing tidying in that
+# directory.
+#
+here_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+benchmark_dir=$1
+
+$here_dir/build-test-harness.sh $benchmark_dir
+
+# COPY ACROSS FIXED TEST HELPER!
+
+rm -rf $benchmark_dir/local-root
