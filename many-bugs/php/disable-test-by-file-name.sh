c241931296d9fb481d187429a24a32e5327aae99
Chris Timperley <christimperley@googlemail.com>
2016-05-12 19:06:20 +0000

    more work on test case disabling.

--- many-bugs/php/disable-test-by-file-name.sh
+++ many-bugs/php/disable-test-by-file-name.sh
@@ -4,9 +4,37 @@
 # for a benchmark whose path is provided as an input,  causing it to return as
 # passed (i.e. exit status 0) without invoking the test itself.
 #
-# Determines the number of the test within the test.pl file, then disables the
-# test case within test.sh which invokes the test with that number.
-#
 # Usage: ./disable-test-by-file-name.sh directory_path test_name
 #
+#
+# TODO: Doesn't deal with cases when the test doesn't exist!
+#
+#
+if [ $# -ne 2 ]; then
+  echo "ERROR: unexpected number of arguments."
+  exit 1
+fi
 
+benchmark=$1
+test_fn=$2
+
+function find_test_num {
+  offset=$(grep "case \$test_id in" "test.sh" -n | cut -d ":" -f1 -)
+  raw=$(grep $1 "test.pl" -n | cut -d ":" -f1 -)
+  num=$(($raw - $offset - 1))
+  echo $num
+}
+
+function disable_test_by_num {
+  sed "s/run tests $1 \&\& exit 0;;/exit 0;;# disabled: $1" 'test.sh'
+}
+
+if [ ! -d $benchmark ]; then
+  echo "ERROR: failed to find benchmark directory: $benchmark"
+  exit 1
+fi
+
+pushd $benchmark > /dev/null
+test_num=$(find_test_num $test_fn)
+disable_test_by_num $test_num
+popd > /dev/null
