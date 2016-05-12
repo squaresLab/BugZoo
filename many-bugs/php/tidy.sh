2dc5b48fe90f15030e194806b353fcd09b1c71d7
Chris Timperley <christimperley@googlemail.com>
2016-05-12 19:31:36 +0000

    added problematic test case disabling into PHP tidy script.

--- many-bugs/php/tidy.sh
+++ many-bugs/php/tidy.sh
@@ -14,3 +14,9 @@ cp $here/php-helper.php $benchmark_dir
 
 rm -rf $benchmark_dir/local-root
 rm -f $benchmark_dir/php-tests-*tar.sh
+
+# Disable problematic tests
+$here/../disable-test-by-file-name.sh $benchmark_dir "ext/standard/tests/time/strptime_basic.phpt"
+$here/../disable-test-by-file-name.sh $benchmark_dir "ext/dom/tests/DOMDocument_validate_external_dtd.phpt"
+$here/../disable-test-by-file-name.sh $benchmark_dir "ext/standard/tests/strings/setlocale_variation2.phpt"
+$here/../disable-test-by-file-name.sh $benchmark_dir "ext/dom/tests/dom007.phpt"
