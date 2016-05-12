f004f2af5eb39042a4c3bd19b365122d8829c3f1
Chris Timperley <christimperley@googlemail.com>
2016-05-12 14:02:14 +0000

    Modified PHP prepare script

--- many-bugs/php/prepare.sh
+++ many-bugs/php/prepare.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-INSTALL_TO="$DIR/local-root"
+dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-if [ ! -d php ]; then
-  mkdir -p local-root/bin
-  tar -xf php.tar.gz
+if [ ! -d src ]; then
+  tar -xf src.tar.gz
   cp libxml.patch php
   gcc php-run-tests.c -o php-run-tests
-  pushd php
+  pushd src
   cat libxml.patch | patch -p0
   rm libxml.patch
-  ./configure --prefix=$INSTALL_TO --build=i686-pc-linux-gnu "CFLAGS=-m32" "CXXFLAGS=-m32" "LDFLAGS=-m32"
+  ./configure --build=i686-pc-linux-gnu "CFLAGS=-m32" "CXXFLAGS=-m32" "LDFLAGS=-m32"
   popd
 fi
