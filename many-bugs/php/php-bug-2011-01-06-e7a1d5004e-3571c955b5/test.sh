4885c44c5dd3f1954f177557e8660417f92f296f
Chris Timperley <christimperley@googlemail.com>
2016-05-18 16:18:01 +0000

    Fixed GenProg files for PHP bug

--- many-bugs/php/php-bug-2011-01-06-e7a1d5004e-3571c955b5/test.sh
+++ many-bugs/php/php-bug-2011-01-06-e7a1d5004e-3571c955b5/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 executable=$( dirname $1 )
 test_id=$2
-dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+here_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 #Check if coverage is being run. If so, don't use time limit.
 if [ `basename $1` = "coverage" ] ; then
@@ -12,8 +12,8 @@ fi
 
 run_test()
 {
-    pushd src > /dev/null
-    timeout $timeout $dir/test.pl $1
+    pushd $here_dir/src > /dev/null
+    timeout $timeout $here_dir/test.pl $1
     result=$?
     popd > /dev/null
     return $result
