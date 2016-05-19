7c193a1c83edb9ec0f7dd8a9f7b8e0ae3bedb8a4
Chris Timperley <christimperley@googlemail.com>
2016-05-19 00:14:27 +0000

    Updated prepare script for lighttpd

--- tse-2012/lighttpd/prepare.sh
+++ tse-2012/lighttpd/prepare.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 here_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+fake_root="$here_dir/fake-root"
 
 pushd $here_dir
 
@@ -9,6 +10,7 @@ gcc hoagie.c -o hoagie
 # Create a fake root and set up a lighttpd conf
 pushd fake-root
   tar -xf htdocs.tar.gz
+  sed "s^__FAKE_ROOT_DIRECTORY__^$fake_root^g" lighttpd.conf.template > lighttpd.conf
 popd
 
 popd
