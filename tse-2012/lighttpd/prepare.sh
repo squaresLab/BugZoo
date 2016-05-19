e0b06c22a9dc05367b3ddde50563c84c781da83b
Chris Timperley <christimperley@googlemail.com>
2016-05-19 00:50:23 +0000

    Updated lighttpd prepare

--- tse-2012/lighttpd/prepare.sh
+++ tse-2012/lighttpd/prepare.sh
@@ -13,4 +13,19 @@ pushd fake-root
   sed "s^__FAKE_ROOT_DIRECTORY__^$fake_root^g" lighttpd.conf.template > lighttpd.conf
 popd
 
+# Unextract, configure, make, and install lighttpd
+tar -xf src.tar.gz
+pushd src
+  ./configure --prefix="$fake_root" --build=i686-gnu-linux "CFLAGS=-m32" "CXXFLAGS=-m32" "LDFLAGS=-m32"
+  make
+  make install
+popd
+
+# Generate the expected output for php.100
+$fake_root/sbin/lighttpd -D -f $fake_root/lighttpd.conf -m $fake_root/lib/ &
+server_process=$!
+sleep 2s
+curl -s "http://localhost:8080/hello.php" | head -100 > test/hello.100
+killall lighttpd
+
 popd
