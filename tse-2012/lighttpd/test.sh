1aae263bbe7e44bd6d6f7a1d2a44d7cc0cd07ca8
Chris Timperley <christimperley@googlemail.com>
2016-05-19 01:03:13 +0000

    updated lighttpd test to work with GenProg 2

--- tse-2012/lighttpd/test.sh
+++ tse-2012/lighttpd/test.sh
@@ -1,34 +1,42 @@
 #!/bin/sh
 #
+# For now, this test script may only be used in a strictly single-threaded
+# environment.
+#
 # TODO: allow different ports to be used
 # TODO: kill JUST the lighttpd server being used for a particular test
 #
-PROG=$1
-OUT=$2
-PORT=$3
+exe=$1
+test_name=$2
+port=$3
 
 here_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 root_dir="$here_dir/fake-root"
 
 # Fire up the server
-# - the conf file is totally wrong!
 $root_dir/sbin/lighttpd -D -f $root_dir/lighttpd.conf -m $root_dir/lib/ &
 sleep 2s
 
-# P1: Fetch index.html
-timeout 1 curl -t 1 -s "http://localhost:8080/index.html" |&
-  diff $here_dir/test/index.html - &> /dev/null && (echo "PASSED P1")
+# Run the appropriate test
+result=1
+case $test_name in
+  p1)
+    timeout 1 curl -t 1 -s "http://localhost:8080/index.html" |& \
+      diff $here_dir/test/index.html - &> /dev/null
+    result=$?;;
+  p2)
+    timeout 1 curl -t 1 -s "http://localhost:8080/hello.php" |& \
+      head -100 |& \
+      diff $here_dir/test/hello.100 - &> /dev/null
+    result=$?;;
+  n1)
+    rm -f exploit
+    timeout 1 $here_dir/hoagie -d localhost -p 8080 -s /hello.php -o exploit &> /dev/null && \
+      diff exploit /etc/passwd &> /dev/null
+    result=$?;;
+esac
 
-# P2: hello.php
-#timeout 1 curl -t 1 -s "http://localhost:8000/hello.php" |&
-#  diff $here_dir/test/hello.php.100 - &> /dev/null && (echo "PASSED P2")
-timeout 1 curl -t 1 -s "http://localhost:8080/hello.php" |&
-  head -100 |&
-  diff $here_dir/test/hello.100 - &> /dev/null && (echo "PASSED P2")
-
-# N1: exploit
-rm -f exploit
-timeout 1 $here_dir/hoagie -d localhost -p 8080 -s /hello.php -o exploit &> /dev/null
-diff exploit /etc/passwd &> /dev/null || (echo "PASSED N1")
-
+# Kill the server
 killall lighttpd
+
+exit $result
