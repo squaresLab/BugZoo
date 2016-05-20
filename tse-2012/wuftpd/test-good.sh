ff2c38a02fa3dc94ec8357bbad3c37591598bf1c
Chris Timperley <christimperley@googlemail.com>
2016-05-20 16:49:19 +0000

    made wuftpd multi-threaded compatible

--- tse-2012/wuftpd/test-good.sh
+++ tse-2012/wuftpd/test-good.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 exe=$1
+exe_dir=$(dirname exe)
 test_name=$2
 port=$3
 here_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 test_dir="$here_dir/test"
 
-# Should move to a safe dir
+# Execute in directory of server
+pushd $exe_dir
 
 # Start the server
 $exe -s -p $port &
+server_pid=$!
 sleep 3s
 
 ftp -in 127.0.0.1 $port < $test_dir/ftp-command-1 |&
@@ -16,11 +19,11 @@ ftp -in 127.0.0.1 $port < $test_dir/ftp-command-1 |&
 
 ftp -in 127.0.0.1 $port < $test_dir/ftp-command-2 |&
   diff $test_dir/ftp-command-2.out - &&
-  diff passwd /etc/passwd && (echo "PASSED P2")
+  diff passwd /etc/passwd && rm passwd && (echo "PASSED P2")
 
 ftp -in 127.0.0.1 $port < $test_dir/ftp-command-3 |&
   diff $test_dir/ftp-command-3.out - &&
-  diff dmesg /bin/dmesg && (echo "PASSED P3")
+  diff dmesg /bin/dmesg && rm dmesg && (echo "PASSED P3")
 
 nc 127.0.0.1 $port < $test_dir/nc-command-4 | grep -v 220 | grep -v Total | grep -v 200- |&
   diff $test_dir/nc-command-4.out - && (echo "PASSED P4")
@@ -34,7 +37,9 @@ timeout 5 $here_dir/wuftpd-god -t 127.0.0.1 -g -w $port -s 4 |&
 timeout 5 $here_dir/wuftpd-god -t 127.0.0.1 -g -w $port -s 4 |&
   grep -q "leave shell" - && (echo "PASSED N2")
 
-# Try to kill JUST the server; this prevents multi-threading :-(
-killall `basename $exe`
+popd
+
+# Kill just this server process
+kill $server_pid
 wait 
 exit 0
