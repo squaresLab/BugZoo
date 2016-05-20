b8609631aa56d62c2ebd9bba25f8bc5508f256ae
Chris Timperley <christimperley@googlemail.com>
2016-05-20 16:32:39 +0000

    updated wuftpd test-good.sh

--- tse-2012/wuftpd/test-good.sh
+++ tse-2012/wuftpd/test-good.sh
@@ -12,23 +12,23 @@ $exe -s -p $port &
 sleep 3s
 
 ftp -in 127.0.0.1 $port < $test_dir/ftp-command-1 |&
-diff $test_dir/ftp-command-1.out - && (echo "PASSED P1")
+  diff $test_dir/ftp-command-1.out - && (echo "PASSED P1")
 
 ftp -in 127.0.0.1 $port < $test_dir/ftp-command-2 |&
-diff $test_dir/ftp-command-2.out - &&
-diff passwd /etc/passwd && (echo "PASSED P2")
+  diff $test_dir/ftp-command-2.out - &&
+  diff passwd /etc/passwd && (echo "PASSED P2")
 
 ftp -in 127.0.0.1 $port < $test_dir/ftp-command-3 |&
-diff $test_dir/ftp-command-3.out - &&
-diff dmesg bin/dmesg && (echo "PASSED P3")
+  diff $test_dir/ftp-command-3.out - &&
+  diff dmesg bin/dmesg && (echo "PASSED P3")
 
-#/usr/bin/nc localhost $PORT < ../nc-command-4 | grep -v 220 | grep -v Total | grep -v 200- >& 4.out
-#diff 4.out ../nc-command-4.out && (echo "nc-4" >> ../$OUT)
-#/usr/bin/nc localhost $PORT < ../nc-command-5 | grep -v 220 >& 5.out
-#diff 5.out ../nc-command-5.out && (echo "nc-5" >> ../$OUT)
+nc 127.0.0.1 $port < $test_dir/nc-command-4 | grep -v 220 | grep -v Total | grep -v 200- |&
+  diff $test_dir/nc-command-4.out - && (echo "PASSED P4")
 
-#kill -9 $! 
+nc 127.0.0.1 $port < $test_dir/nc-command-5 | grep -v 220 |&
+  diff $test_dir/nc-command-5.out - && (echo "PASSED P5")
+
+# Try to kill JUST the server; this prevents multi-threading :-(
 killall `basename $exe`
-#killall -9 `basename $exe`
 wait 
 exit 0
