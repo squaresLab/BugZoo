c78020974d2b0703b8804dcdebf518811558a42a
Chris Timperley <christimperley@googlemail.com>
2016-05-20 17:00:56 +0000

    updated wuftpd

--- tse-2012/wuftpd/test.sh
+++ tse-2012/wuftpd/test.sh
@@ -8,6 +8,7 @@ test_dir="$here_dir/test"
 
 # Are we running coverage?
 [[ $(basename $exe) = "coverage" ]] && is_cov=0 || is_cov=1
+$is_cov && timeout=30 || timeout=5
 
 # Execute in directory of server
 pushd $exe_dir
@@ -19,27 +20,24 @@ sleep 2s
 
 result=1
 case $test_name in
-  p1) ftp -in 127.0.0.1 $port < $test_dir/ftp-command-1 |&
+  p1) timeout $timeout ftp -in 127.0.0.1 $port < $test_dir/ftp-command-1 |&
         diff $test_dir/ftp-command-1.out - && result=0;;
 
-  p2) ftp -in 127.0.0.1 $port < $test_dir/ftp-command-2 |&
+  p2) timeout $timeout ftp -in 127.0.0.1 $port < $test_dir/ftp-command-2 |&
         diff $test_dir/ftp-command-2.out - &&
         diff passwd /etc/passwd && rm passwd && result=0;;
 
-  p3) ftp -in 127.0.0.1 $port < $test_dir/ftp-command-3 |&
+  p3) timeout $timeout ftp -in 127.0.0.1 $port < $test_dir/ftp-command-3 |&
        diff $test_dir/ftp-command-3.out - &&
        diff dmesg /bin/dmesg && rm dmesg && result=0;;
 
-  p4) nc 127.0.0.1 $port < $test_dir/nc-command-4 | grep -v 220 | grep -v Total | grep -v 200- |&
+  p4) timeout $timeout nc 127.0.0.1 $port < $test_dir/nc-command-4 | grep -v 220 | grep -v Total | grep -v 200- |&
         diff $test_dir/nc-command-4.out - && result=0;;
 
-  p5) nc 127.0.0.1 $port < $test_dir/nc-command-5 | grep -v 220 |&
+  p5) timeout $timeout nc 127.0.0.1 $port < $test_dir/nc-command-5 | grep -v 220 |&
         diff $test_dir/nc-command-5.out - && result=0;;
 
-  n1) timeout 5 $here_dir/wuftpd-god -t 127.0.0.1 -g -w $port -s 4 |&
-        grep -q "leave shell" - && result=0;;
-
-  n2) timeout 5 $here_dir/wuftpd-god -t 127.0.0.1 -g -w $port -s 4 |&
+  n1) timeout $timeout $here_dir/wuftpd-god -t 127.0.0.1 -g -w $port -s 4 |&
         grep -q "leave shell" - && result=0;;
 esac
 popd
