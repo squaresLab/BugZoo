49049030b36fcddf6376171eb18a7efbfb91eab4
Chris Timperley <christimperley@googlemail.com>
2016-05-20 16:20:43 +0000

    added prepare script to wuftpd

--- tse-2012/wuftpd/compile.sh
+++ tse-2012/wuftpd/compile.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 exe=$1
 exe_dir=$(dirname $exe)
 here_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
--- tse-2012/wuftpd/prepare.sh
+++ tse-2012/wuftpd/prepare.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 here_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 test_dir="$here_dir/test"
+oracle=$here_dir/oracle/ftpd
+port=8931
 
-# Generate the expected outputs for each of the commands
-$exe -s -p $port &
+# Compile an oracle ftpd and generate the expected output for each of the
+# commands
+./compile.sh $oracle
+
+$oracle  -s -p $port &
 sleep 5s
 
 ftp -in 127.0.0.1 $port < $test_dir/ftp-command-1 &> $test_dir/ftp-command-1.out
+ftp -in 127.0.0.1 $port < $test_dir/ftp-command-2 &> $test_dir/ftp-command-2.out
+#rm -f passwd
 
+killall $(basename $oracle)
+wait
