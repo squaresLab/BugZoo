2c097f35882da11d7a790ad2537ca67feb7c6b57
Chris Timperley <christimperley@googlemail.com>
2016-05-23 14:46:18 +0000

    fixed tiff prepare

--- tse-2012/tiff/prepare.sh
+++ tse-2012/tiff/prepare.sh
@@ -3,6 +3,6 @@ if [ ! -d src ]; then
   tar -xf src.tar.gz
   mkdir -p local-root
   pushd src
-  ./configure --prefix="$PWD/local-root" --host=i686-linux-gnu
+  ./configure --prefix="$PWD/local-root" --host=i686-linux-gnu "CFLAGS=-m32" "CXXFLAGS=-m32" "LDFLAGS=-m32"
   popd
 fi
