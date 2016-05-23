9d9b6694ca5edd277b1fc28c779a413e7b9ed700
Chris Timperley <christimperley@googlemail.com>
2016-05-23 15:31:46 +0000

    added compile script for tiff

--- tse-2012/tiff/compile.sh
+++ tse-2012/tiff/compile.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 exe=$1
-exe_dir=$(dirname $1)
+patch_dir=$(dirname $1)
+here_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+project_dir="$here_dir/src"
+
+cp $patch_dir/lib_lzw.c $project_dir/lib_lzw.c
+pushd $project_dir
+make install && exit 0
+exit 1
