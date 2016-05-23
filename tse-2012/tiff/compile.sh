a6932860e6712c76473b57fd886e82d3deeea54a
Chris Timperley <christimperley@googlemail.com>
2016-05-23 16:03:31 +0000

    updated tiff compile; may still need make clean

--- tse-2012/tiff/compile.sh
+++ tse-2012/tiff/compile.sh
@@ -3,8 +3,10 @@ exe=$1
 patch_dir=$(dirname $1)
 here_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 project_dir="$here_dir/src"
+target_dir="$project_dir/libtiff"
 
-cp $patch_dir/lib_lzw.c $project_dir/lib_lzw.c
+#rm -f $target_dir/tif_lzw.*
+cp $target_dir/tif_lzw.c $target_dir/tif_lzw.c
 pushd $project_dir
 make install && exit 0
 exit 1
