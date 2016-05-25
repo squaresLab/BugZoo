a485fdd8e2cfd75ed0f61790876f1cd2b6dbea78
Chris Timperley <christimperley@googlemail.com>
2016-05-25 17:43:28 +0000

    computed coverage for another PHP bug

--- many-bugs/php/php-bug-2011-02-01-01745fa657-1f49902999/prepare.sh
+++ many-bugs/php/php-bug-2011-02-01-01745fa657-1f49902999/prepare.sh
@@ -10,3 +10,7 @@ if [ ! -d src ]; then
   ./configure --host=i686-pc-linux-gnu "CFLAGS=-m32 -std=gnu99" "CXXFLAGS=-m32" "LDFLAGS=-m32"
   popd
 fi
+
+if [ ! -d coverage ]; then
+  tar -xf coverage.tar.gz
+fi
