ca377a331cfedc1b9e0f68e203c74cb0dbe9f282
Chris Timperley <christimperley@googlemail.com>
2016-05-25 19:42:52 +0000

    computed coverage for another Python bug

--- many-bugs/python/python-bug-69368-69372/test.pl
+++ many-bugs/python/python-bug-69368-69372/test.pl
@@ -4,7 +4,7 @@ use warnings;
 use Cwd;
 use File::Basename;
 
-my @TESTS = (
+my @tests = (
     "test_grammar",
     "test_opcodes",
     "test_dict",
@@ -340,8 +340,7 @@ if (length($ARGV[0]) == 0)
     die "Must specify a test number";
 }
 
-my $test = $TESTS[$ARGV[0] - 1];
-my $orig_dir = cwd;
+my $test = $tests[$ARGV[0] - 1];
 my $project_dir = Cwd::abs_path(dirname(__FILE__)) . "/src";
 my $exe = "$project_dir/python";
 
