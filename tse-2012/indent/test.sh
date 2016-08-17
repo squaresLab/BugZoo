d707ada0637d9d5d894530ed4fa6731b64bc8aac
ChrisTimperley <christimperley@googlemail.com>
2016-08-17 23:54:24 +0100

    fixed indent scripts

--- tse-2012/indent/test.sh
+++ tse-2012/indent/test.sh
@@ -3,8 +3,8 @@ executable=$1
 test_id=$2
 here_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-# Are we computing coverage?
-[[ $(basename $1) = "coverage" ]] && coverage=0 || coverage=1
+# Check if this test script is being used to compute coverage information.
+[[ $(dirname $executable) = "coverage" ]] && coverage=0 || coverage=1
 
 positive()
 {
