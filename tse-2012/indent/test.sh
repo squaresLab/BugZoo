fb162af4b617b7658ce5d06da2967b007a7ee3c6
ChrisTimperley <christimperley@googlemail.com>
2016-08-17 03:37:36 +0100

    refactored indent test script

--- tse-2012/indent/test.sh
+++ tse-2012/indent/test.sh
@@ -1,46 +1,30 @@
 #!/bin/bash
-EXECUTABLE=$1
-TEST_ID=$2
+executable=$1
+test_id=$2
 here_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-# Check if this test script is being used to compute coverage information.
-if [ $(basename $1) = "coverage" ]; then
-  cov=1
-else
-  cov=0
-fi
+# Are we computing coverage?
+[[ $(basename $1) = "coverage" ]] && coverage=0 || coverage=1
 
-# Treats the test case as a positive test case.
-exec_pos()
+positive()
 {
-  if [ $cov = 0 ]; then
-    timeout 2 bash -c "$EXECUTABLE < $here_dir/test/$TEST_ID" \
-      |& diff $here_dir/test/output.$TEST_ID - &> /dev/null
-  else
-    bash -c "$EXECUTABLE < $DIR/test/$TEST_ID" \
-      |& diff $here_dir/test/output.$TEST_ID - &> /dev/null
-  fi
-  return $?
+  [[ $coverage = 0 ]] && timeout=60 || timeout=2
+  timeout $timeout $executable < $here_dir/test/$test_id \
+    |& diff $here_dir/test/output.$test_id - &> /dev/null
 }
 
-# Treats the test case as a negative test case.
-exec_neg()
+negative()
 {
-  if [ $cov = 0 ]; then
-    timeout 1 $EXECUTABLE < $here_dir/test/n1 &> /dev/null
-  else
-    timeout 10 $EXECUTABLE < $here_dir/test/n1 &> /dev/null
-  fi
-  return $?
+  [[ $coverage = 0 ]] && timeout=10 || timeout=1
+  timeout $timeout $executable < $here_dir/test/n1 &> /dev/null
 }
 
-# Execute the appropriate test case.
-case $TEST_ID in
-  p1) exec_pos && exit 0;;
-  p2) exec_pos && exit 0;;
-  p3) exec_pos && exit 0;;
-  p4) exec_pos && exit 0;;
-  p5) exec_pos && exit 0;;
-  n1) exec_neg && exit 0;;
+case $test_id in
+  p1) positive && exit 0;;
+  p2) positive && exit 0;;
+  p3) positive && exit 0;;
+  p4) positive && exit 0;;
+  p5) positive && exit 0;;
+  n1) negative && exit 0;;
 esac
 exit 1
