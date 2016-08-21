5d2594ae59e2c13d59c0afd1ccfcea4ca57e97e9
Chris Timperley <christimperley@googlemail.com>
2016-08-21 21:24:56 +0100

    ImageMagick now compiles

--- tse-2012/imagemagick/prepare.sh
+++ tse-2012/imagemagick/prepare.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+here_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+fake_root="$here_dir/fake-root"
+mkdir -p $fake_root
+
 if [ ! -d src ]; then
   tar -xf src.tar.gz
   cp blob.c.fixed src/magick/blob.c
   pushd src
-  ./configure --build=i386-pc-linux-gnu
-  make
+  ./configure --build=i386-pc-linux-gnu --prefix="$fake_root" --with-perl=no && \
+    make && make install
   popd
 fi
