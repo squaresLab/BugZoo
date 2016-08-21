6afdf197417c0979166dd3612dd5bd1cfb3582af
Chris Timperley <christimperley@googlemail.com>
2016-08-21 15:19:28 +0100

    almost finished PHP test harness

--- tse-2012/php/test.sh
+++ tse-2012/php/test.sh
@@ -3,16 +3,16 @@ executable=$1
 test_id=$2
 here_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 test_dir="$here_dir/test"
-php_exe="$here_dir/src/"
+php_exe="$here_dir/src/sapi/cli/php"
 
 # Check if this test script is being used to compute coverage information.
 coverage=$([[ $(basename $executable) = "coverage" ]])
 #[[ $coverage = 0 ]] && timeout=10 || timeout=q0
 
 positive(){
-   "$test_dir/$test_id.php" | head -n 100 | diff "$test_dir/$test_id.out" -
+   $php_exe "$test_dir/$test_id.php" | head -n 100 | diff "$test_dir/$test_id.out" -
 }
 
 negative(){
-  $PHPROOT/php "$test_dir/n1.php" &| diff "$test_dir/n1.out" -
+  $php_exe "$test_dir/n1.php" &| diff "$test_dir/n1.out" -
 }
