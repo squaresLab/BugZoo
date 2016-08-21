8c1e8b3ac71b50c29046e9d5f2f09a339648a79e
Chris Timperley <christimperley@googlemail.com>
2016-08-21 21:28:19 +0100

    working on IM test harness

--- tse-2012/imagemagick/test-good.sh
+++ tse-2012/imagemagick/test-good.sh
@@ -1,12 +1,20 @@
-#!/bin/sh
-ulimit -t 10
-MAGICKROOT=/home/claire/imagemagick/magick-root/bin/
+#!/bin/bash
+executable=$1
+test_id=$2
+here_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+test_dir="$here_dir/test"
+fake_root="$here_dir/fake-root"
+
+# Check if this test script is being used to compute coverage information.
+[[ $(dirname $executable) = "coverage" ]] && coverage=0 || coverage=1
+[[ $coverage = 0 ]] && timeout=10 || timeout=2
+
 
 for png in BASI0G01 BASN3P01 G10N2C08 S37N3P04 TBWN3P08 
 do
   rm -rf gradient.png tmp.txt $png".txt"
-  ./limit10 $MAGICKROOT/convert "pngsuite/"$png".PNG" -function Arcsin 4 gradient.png
-  ./limit10 $MAGICKROOT/identify -verbose gradient.png > tmp.txt
+  timeout $timeout $fake_root/convert "pngsuite/"$png".PNG" -function Arcsin 4 gradient.png
+  timeout $timeout $fake_root/identify -verbose gradient.png > tmp.txt
   head -n 22 tmp.txt | tail -n 6 > $png".txt"
   diff $png".txt" "test_"$png".out" && echo "passed "$png".PNG" >> $2
 done
