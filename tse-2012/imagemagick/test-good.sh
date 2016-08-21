c1b0a94ffa902bb69b8d10f38b7a442723f1809a
Chris Timperley <christimperley@googlemail.com>
2016-08-21 21:59:37 +0100

    more work on IM harness

--- tse-2012/imagemagick/test-good.sh
+++ tse-2012/imagemagick/test-good.sh
@@ -3,22 +3,20 @@ executable=$1
 test_id=$2
 here_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 test_dir="$here_dir/test"
+image_dir="$here_dir/images"
 fake_root="$here_dir/fake-root"
 
 # Check if this test script is being used to compute coverage information.
 [[ $(dirname $executable) = "coverage" ]] && coverage=0 || coverage=1
 [[ $coverage = 0 ]] && timeout=10 || timeout=2
 
-
 for png in BASI0G01 BASN3P01 G10N2C08 S37N3P04 TBWN3P08 
 do
-  rm -rf gradient.png tmp.txt $png".txt"
-  timeout $timeout $fake_root/convert "pngsuite/"$png".PNG" -function Arcsin 4 gradient.png
-  timeout $timeout $fake_root/identify -verbose gradient.png > tmp.txt
-  head -n 22 tmp.txt | tail -n 6 > $png".txt"
-  diff $png".txt" "test_"$png".out" && echo "passed "$png".PNG" >> $2
+  tmp_image_file=$(mktemp)
+  timeout $timeout $fake_root/bin/convert "$image_dir/$png.PNG" -function Arcsin 4 $tmp_image_file && \
+  timeout $timeout $fake_root/bin/identify -verbose $tmp_image_file | head -n 22 | tail -n 6 |& \
+  diff "$test_dir/test_"$png".out" - && echo "passed "$png".PNG"
+  rm -f $tmp_image_file
 done
-
-rm TBWN3P08.txt tmp.txt gradient.png
 
 exit 0
