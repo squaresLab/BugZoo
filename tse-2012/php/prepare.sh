24e5b2dc0003860d428e297305a56fd9d3ed1ad8
Chris Timperley <christimperley@googlemail.com>
2016-08-21 15:15:39 +0100

    fixed PHP TSE prepare

--- tse-2012/php/prepare.sh
+++ tse-2012/php/prepare.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-if [ -d src ]; then
+here_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+# Unextract the source code, configure, and compile
+if [ ! -d src ]; then
   tar -xf src.tar.gz
+  cp libxml.patch src
   pushd src
-  ./configure --host=i386-pc-linux-gnu "CFLAGS=-m32" "CXXFLAGS=-m32" "LDFLAGS=-m32" 
+  ./configure --host=i386-pc-linux-gnu "CFLAGS=-m32"
+  cat libxml.patch | patch -p0 && make
 fi
+
+# Generate expected test outputs for positive tests
+#php_exe="$here_dir/"
--- tse-2012/php/test.sh
+++ tse-2012/php/test.sh
@@ -3,13 +3,14 @@ executable=$1
 test_id=$2
 here_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 test_dir="$here_dir/test"
+php_exe="$here_dir/src/"
 
 # Check if this test script is being used to compute coverage information.
 coverage=$([[ $(basename $executable) = "coverage" ]])
-[[ $coverage = 0 ]] && timeout=10 || timeout=1
+#[[ $coverage = 0 ]] && timeout=10 || timeout=q0
 
 positive(){
-  $PHPROOT/php "$test_dir/$test_id.php" | head -n 100 | diff "$test_dir/$test_id.out" -
+   "$test_dir/$test_id.php" | head -n 100 | diff "$test_dir/$test_id.out" -
 }
 
 negative(){
