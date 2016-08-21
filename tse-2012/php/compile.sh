34b6cb46ff52db46fcd82c2e3c11af74887869ca
Chris Timperley <christimperley@googlemail.com>
2016-08-21 14:28:32 +0100

    updated PHP TSE compile script

---
+++ tse-2012/php/compile.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+TIME_LIMIT=15
+patch_dir=$(dirname "$1")
+here_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+project_dir="$here_dir/src"
+
+rm -f $project_dir/string.*
+cp $patch_dir/string.c $project_dir/ext/standard/string.c
+
+pushd $project_dir
+make clean && timeout $TIME_LIMIT make && exit 0
+exit 1
