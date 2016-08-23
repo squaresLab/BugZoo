0459e3785f764a141cdddc1f4697ee489bf70422
ChrisTimperley <christimperley@googlemail.com>
2016-08-23 18:23:55 +0000

    fixed coverage check in deroff test harness

--- tse-2012/deroff/test.sh
+++ tse-2012/deroff/test.sh
@@ -5,7 +5,7 @@ here_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 test_dir="$here_dir/test"
 
 # Check if this test script is being used to compute coverage information.
-[[ $(dirname $executable) = "coverage" ]] && coverage=0 || coverage=1
+[[ $(dirname "$executable") = "coverage" ]] && coverage=0 || coverage=1
 [[ $coverage = 0 ]] && timeout=10 || timeout=1
 
 # Perform necessary preparations before running the test case.
