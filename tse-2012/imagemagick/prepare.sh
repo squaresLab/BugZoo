0aa504f47a92ad51d7d5388c9daedeb6f977d381
ChrisTimperley <christimperley@googlemail.com>
2016-08-23 18:49:14 +0000

    fixed imagemagick prepare; works on repairbox

--- tse-2012/imagemagick/prepare.sh
+++ tse-2012/imagemagick/prepare.sh
@@ -7,8 +7,8 @@ if [ ! -d src ]; then
   tar -xf src.tar.gz
   cp blob.c.fixed src/magick/blob.c
   pushd src
-  ./configure --build=i386-pc-linux-gnu --prefix="$fake_root" --with-perl=no && \
-    make && make install
+  ./configure --build=i386-pc-linux-gnu --prefix="$fake_root" --with-perl=no CFLAGS="-m32 -std=c99" CXXFLAGS="-m32" LDFLAGS="-m32" && \
+   make && make install
   popd
 fi
 
