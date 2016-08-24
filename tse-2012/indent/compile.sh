897fcd805268fc7b2d782579cb547dcf3710a8a4
ChrisTimperley <christimperley@googlemail.com>
2016-08-24 12:17:14 +0000

    fixed indent to work on Docker

--- tse-2012/indent/compile.sh
+++ tse-2012/indent/compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 TIME_LIMIT=10
-exe_name=$1
-exe_dir=$(dirname $exe_name)
-timeout $TIME_LIMIT gcc $exe_dir/indent_comb.c -o $exe_name -m32 && exit 0
+exe_dir=$(dirname "$exe_name")
+exe_name="$exe_dir/indent"
+timeout $TIME_LIMIT gcc "$exe_dir/indent.c" -o "$exe_name" -m32 -std=c90 && exit 0
 exit 1
