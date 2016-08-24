fcd613efb344043a1ea69371681d60eab22ef95e
Chris Timperley <christimperley@googlemail.com>
2016-08-24 01:12:09 +0100

    almost finished nullhttpd test harness

--- tse-2012/nullhttpd/test.sh
+++ tse-2012/nullhttpd/test.sh
@@ -10,7 +10,7 @@ server_url="http://localhost:"$port""
 
 # Check if this test script is being used to compute coverage information.
 coverage=$([[ "$executable_dir" = "coverage" ]])
-[[ $coverage = 0 ]] && timeout=30 || timeout=30
+[[ $coverage = 0 ]] && sleepytime=15 || sleepytime=1
 
 build_root(){
   root_dir=$1
@@ -32,28 +32,28 @@ build_root $server_dir $port
 # Get the server running?
 pushd $server_dir/bin
 sudo $executable $port &
-sleep 1s
+sleep "$sleepytime"s
 
-timeout $timeout curl --silent -t 1 "$server_url/index.html" |& \
+timeout 1 curl --silent -t 1 "$server_url/index.html" |& \
 diff $test_dir/index.html - &> /dev/null && echo "index.html"
 
-timeout $timeout curl --silent -t 1 "$server_url/blank.html" |& \
+timeout 1 curl --silent -t 1 "$server_url/blank.html" |& \
 diff $test_dir/blank.html - && echo "blank.html"
 
-timeout $timeout curl --silent -t 1 "$server_url/notfound.html" |& \
+timeout 1 curl --silent -t 1 "$server_url/notfound.html" |& \
 diff $test_dir/notfound.html - && echo "notfound.html"
 
-timeout $timeout curl --silent -t 1 "$server_url/images/default.gif" |& \
+timeout 1 curl --silent -t 1 "$server_url/images/default.gif" |& \
 diff $test_dir/default.gif - && echo "default.gif"
 
-timeout $timeout curl --silent -t 1 "$server_url/images/" |& head -n 4 |& \
+timeout 1 curl --silent -t 1 "$server_url/images/" |& head -n 4 |& \
 diff $test_dir/images.html - && echo "images.html"
 
-timeout $timeout wget -O - -o /dev/null -t 1 --post-data 'name=westley&submit=submit' "$server_url/cgi-bin/hello.pl" |& \
+timeout 1 wget -O - -o /dev/null -t 1 --post-data 'name=westley&submit=submit' "$server_url/cgi-bin/hello.pl" |& \
 diff $test_dir/hello.out - && echo "hello.pl"
 
-$test_dir/exploit -h localhost -p $port -t2 && \
-timeout $timeout curl --silent -t 1 "$server_url/index.html" |& \
+timeout 5 $test_dir/exploit -h localhost -p $port -t2
+timeout 1 curl --silent -t 1 "$server_url/index.html" |& \
 diff $test_dir/index.html - && echo "exploit"
 
 # Destroy the temporary root and kill the server
