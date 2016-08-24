eb4798bba52623ad8a55b7556a563ddd6ac598ef
ChrisTimperley <christimperley@googlemail.com>
2016-08-24 12:20:12 +0000

    improved indent test harness

--- tse-2012/indent/test.sh
+++ tse-2012/indent/test.sh
@@ -4,18 +4,18 @@ test_id=$2
 here_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 # Check if this test script is being used to compute coverage information.
-[[ $(dirname $executable) = "coverage" ]] && coverage=0 || coverage=1
+[[ $(dirname "$executable") = "coverage" ]] && coverage=0 || coverage=1
 
 positive()
 {
-  [[ $coverage = 0 ]] && timeout=60 || timeout=2
+  [[ $coverage = 0 ]] && timeout=30 || timeout=1
   timeout $timeout $executable < $here_dir/test/$test_id \
     |& diff $here_dir/test/$test_id.out - &> /dev/null
 }
 
 negative()
 {
-  [[ $coverage = 0 ]] && timeout=10 || timeout=2
+  [[ $coverage = 0 ]] && timeout=30 || timeout=1
   timeout $timeout $executable < $here_dir/test/n1 &> /dev/null
 }
 
