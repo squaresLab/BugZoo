3cf8abda295a659bf56d51de3ed959daa11d2d47
Chris Timperley <christimperley@googlemail.com>
2016-09-01 20:57:04 +0100

    added another python bug

---
+++ many-bugs/python/python-bug-69783-69784/prepare.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ ! -d src ]; then
+  tar -xf src.tar.gz
+  pushd src
+  make clean
+  ./configure --host=i686-pc-linux-gnu --with-dbmliborder=gdbm --with-threads "CFLAGS=-m32 -std=gnu99 -fgnu89-inline" "CXXFLAGS=-m32" "LDFLAGS=-m32"
+  popd
+fi
