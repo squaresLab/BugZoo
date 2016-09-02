87578cfb3ad13962df6d083d51a8227d9077ee98
ChrisTimperley <christimperley@googlemail.com>
2016-09-02 14:22:52 +0000

    updated gzip-1204630c96

---
+++ many-bugs/gzip/gzip-bug-2010-01-30-fc00329e3d-1204630c96/compile.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+TIME_LIMIT=60
+HERE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+PATCH_EXE=$1
+PATCH_DIR=$(dirname "$PATCH_EXE")
+PROJECT_DIR="$HERE_DIR/src"
+
+# Remove the object file for the affected source code file
+cp "$PATCH_DIR/inflate.c" "$PROJECT_DIR/inflate.c" && \
+pushd "$PROJECT_DIR" && \
+rm -f inflate.o && \
+
+# Clear the results of any test executions for any previous patches
+pushd tests && \
+make clean && \
+popd && \
+
+# Rebuild the rest of the project
+timeout $TIME_LIMIT make || exit 1
