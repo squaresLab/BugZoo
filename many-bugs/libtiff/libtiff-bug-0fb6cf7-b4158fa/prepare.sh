6d88cfd694eb6bce0ccfe2edadfb7e9b3005e81f
ChrisTimperley <christimperley@googlemail.com>
2016-09-02 23:48:04 +0000

    fixed libtiff-b4158fa

--- many-bugs/libtiff/libtiff-bug-0fb6cf7-b4158fa/prepare.sh
+++ many-bugs/libtiff/libtiff-bug-0fb6cf7-b4158fa/prepare.sh
@@ -3,7 +3,8 @@ if [ ! -d src ]; then
   tar -xf src.tar.gz
   pushd src
   ./autogen.sh
-  ./configure --host=i686-pc-linux-gnu "CFLAGS=-m32" "CXXFLAGS=-m32" "LDFLAGS=-m32"
-  make
+  ./configure#"CFLAGS=-m32" "CXXFLAGS=-m32" "LDFLAGS=-m32"
+  make clean
   popd
+  ./compile.sh preprocessed/libtiff 
 fi
