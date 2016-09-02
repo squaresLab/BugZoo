19567196dc6d604e039b72fff04d217a0fc353c0
ChrisTimperley <christimperley@googlemail.com>
2016-09-02 13:42:32 +0000

    updated gzip-884ef6d16c

--- many-bugs/gzip/gzip-bug-2010-02-19-3eb6091d69-884ef6d16c/prepare.sh
+++ many-bugs/gzip/gzip-bug-2010-02-19-3eb6091d69-884ef6d16c/prepare.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
-if [ ! -d gzip ]; then
-  tar -xf gzip.tar.gz
-  pushd gzip
+HERE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ) 
+SRC_DIR="$HERE_DIR/src"
+BENCHMARK_NAME=$(basename "$HERE_DIR")
+
+pushd "$HERE_DIR"
+if [ ! -d src ]; then
+  # Unextract and compile source
+  tar -xf src.tar.gz
+  pushd src
   make clean
   ./configure --build=i686-pc-linux-gnu "CFLAGS=-m32" "CXXFLAGS=-m32" "LDFLAGS=-m32"
+  patch lib/stdio.in.h < ../stdio.in.h.patch && make
   popd
-  cp ../stdio.patch .
-  patch gzip/lib/stdio.h < stdio.patch
-  rm -f stdio.patch
-fi
 
-if [ ! -d tests ]; then
-  tar -xf tests.tar.gz
+  # Unextract the tests, move into the source directory, and fix all references
+  # to any previous absolute paths to the source directory
+  pushd src/tests
+  make clean
+  sed -i.bk "s#/root/mountpoint-genprog/genprog-many-bugs/$BENCHMARK_NAME/gzip#$SRC_DIR#g" "$SRC_DIR/tests/Makefile"
+  popd
 fi
