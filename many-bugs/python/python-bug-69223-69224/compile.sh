95ccc2e16441430115d8842606902000d22fc28a
Chris Timperley <christimperley@googlemail.com>
2016-11-02 12:49:52 -0400

    optimised Python-69224 compilation script

--- many-bugs/python/python-bug-69223-69224/compile.sh
+++ many-bugs/python/python-bug-69223-69224/compile.sh
@@ -32,8 +32,15 @@ if !(cp $candidate_dir/* $src_dir -rf); then
   exit 1
 fi
 
+# Destroy all temporary object files and shared libraries related to the
+# affected files
 pushd "$src_dir"
-if !(timeout $TIME_LIMIT make clean && make); then
+rm -f python
+rm -f build/temp.linux-x86_64-3.3/experiment/src/Modules/selectmodule.o
+rm -f build/lib.linux-x86_64-3.3/select.cpython-33m.so
+
+# Rebuild!
+if !(timeout $TIME_LIMIT make); then
   echo "ERROR: failed to execute make within problem src directory"
   exit 1
 fi
