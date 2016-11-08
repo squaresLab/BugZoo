5902885b66bda18d316c74fccb5b931e40d77028
Chris Timperley <christimperley@googlemail.com>
2016-11-08 15:14:55 -0500

    updated rewriter.sh to generate test.commands

--- sir/rewriter.sh
+++ sir/rewriter.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 #
+# Generates a file called "test.commands", where each line describes the
+# command that should be executed for the test case matching that line
+# number (i.e. the first line corresponds to the command for t1).
+#
 # Requires: sponge (provided by manyutils on Linux and OSX)
 
 # Remove the boilerplate at the top of the file
-tail -n +11 test.sh > tester.sh
+tail -n +11 test.sh > test.commands
 
 # Replace all instances of the executable with $EXECUTABLE, then keep only
 # those lines in the file
-sed -i 's#../source/grep.exe#$EXECUTABLE#g' tester.sh
-sed -i 's#../inputs/#$INPUTS/#g' tester.sh
-grep '^$EXECUTABLE' tester.sh | sponge tester.sh
-sed -i 's#> ../outputs/.*$##g' tester.sh
-
-# Make sure the new test script is executable
-chmod +x tester.sh
-
-# Debugging
-#less tester.sh
+sed -i 's#../source/grep.exe#$EXECUTABLE#g' test.commands
+sed -i 's#../inputs/#$INPUTS/#g' test.commands
+grep '^$EXECUTABLE' test.commands | sponge test.commands
+sed -i 's#> ../outputs/.*$##g' test.commands
