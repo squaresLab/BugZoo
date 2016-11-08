66455c1752cb87afddf1614f3127a34571badffe
Chris Timperley <christimperley@googlemail.com>
2016-11-08 14:50:15 -0500

    begun work on SIR Dockerfiles

---
+++ sir/rewriter.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Remove the boilerplate at the top of the file
+tail -n +11 test.sh > tester.sh
+
+# Replace all instances of the executable with $EXECUTABLE, then keep only
+# those lines in the file
+sed -i 's#../source/grep.exe#$EXECUTABLE#g' tester.sh
+grep '^$EXECUTABLE' tester.sh | sponge tester.sh
+
+# Make sure the new test script is executable
+chmod +x tester.sh
+
+# Debugging
+less tester.sh
