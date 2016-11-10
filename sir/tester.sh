55e3e748af6780d1587e4cd3bc00ab55d1064eaf
Chris Timperley <christimperley@googlemail.com>
2016-11-09 21:38:37 -0500

    added base image for SIR benchmarks

--- sir/tester.sh
+++ sir/tester.sh
@@ -9,6 +9,14 @@ HERE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 # Try to find the test number matching the test descriptor.
 # If there is no matching test case, print an error and exit with code 1.
-if NOT_FOUND; then
+NOT_FOUND=1
+if [[ $NOT_FOUND -eq 0 ]]; then
   echo "Failed to run test $TEST: no corresponding test number"; exit 1
 fi
+
+# The test number for this test is now stored in TEST_NUMBER
+TEST_NUMBER=1
+
+# Find the command for this test, then inject the environmental variables
+TEST_COMMAND=$(sed -n "${TEST_NUMBER}p" "${HERE_DIR}/test.commands")
+echo $TEST_COMMAND
