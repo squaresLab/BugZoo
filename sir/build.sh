1a776c675f56e74a68f5dcfa969642c0b8c4c504
Chris Timperley <christimperley@googlemail.com>
2016-11-12 19:14:07 -0500

    added description to build functions

--- sir/build.sh
+++ sir/build.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 HERE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
+# Constructs the Docker image for a particular SIR program.
 build_program(){
   program=$1
   program_image="repairbox:sir-${program}"
   docker build -t ${program_image} "${HERE_DIR}/${program}"
 }
 
+# Constructs the Docker image for a particular version of a program within
+# the SIR benchmarks.
 build_program_version(){
   program=$1
   version=$2
@@ -32,10 +35,14 @@ build_program_version(){
   rm -rf ${docker_dir}
 }
 
+# Builds the Docker image for a particular fault within the SIR benchmarks.
+build_fault(){
+  program=$1
+  version=$2
+  fault=$3
+}
+
+
 build_program grep
 
 build_program_version grep v1
-
-#docker build -t repairbox:sir .
-#docker build -t repairbox:sir-grep grep
-#docker build -t repairbox:sir-grep-v1-f1 example-fault
