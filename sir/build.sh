85b8e5e1432a4edfb758e3c2d62aa635b3745582
Chris Timperley <christimperley@googlemail.com>
2016-11-12 19:11:23 -0500

    added cool functions to SIR build script

--- sir/build.sh
+++ sir/build.sh
@@ -1,4 +1,41 @@
 #!/bin/bash
-docker build -t repairbox:sir .
-docker build -t repairbox:sir-grep grep
-docker build -t repairbox:sir-grep-v1-f1 example-fault
+HERE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+build_program(){
+  program=$1
+  program_image="repairbox:sir-${program}"
+  docker build -t ${program_image} "${HERE_DIR}/${program}"
+}
+
+build_program_version(){
+  program=$1
+  version=$2
+
+  # Compute the name of the base image for the program
+  program_image="repairbox:sir-${program}"
+
+  # Compute the name of the image for this version
+  version_image="${program_image}-v${version}"
+
+  # Construct the image for this program version
+  docker_dir=$(mktemp -d)
+  docker_file="${docker_dir}/Dockerfile"
+  cp ${HERE_DIR}/Dockerfile.version ${docker_file}
+  sed -i "s#^FROM <BASE_FILE>\$#FROM ${program_image}#" ${docker_file}
+  sed -i "s#<PROGRAM_VERSION>#${version}#" ${docker_file}
+  cat ${docker_file}
+
+  # Build the docker image
+  docker build -t ${version_image} ${docker_dir}
+
+  # Destroy the temporary directory
+  rm -rf ${docker_dir}
+}
+
+build_program grep
+
+build_program_version grep v1
+
+#docker build -t repairbox:sir .
+#docker build -t repairbox:sir-grep grep
+#docker build -t repairbox:sir-grep-v1-f1 example-fault
