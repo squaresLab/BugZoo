d6344a016237094b77bd2f45230ab7af42a99bb9
Chris Timperley <christimperley@googlemail.com>
2016-11-19 15:41:25 -0500

    added PHP configs and diffs

--- many-bugs/php/get-diffs-and-configs.sh
+++ many-bugs/php/get-diffs-and-configs.sh
@@ -6,9 +6,15 @@ fetch_bug(){
   pushd /tmp &&\
     wget -qq "http://repairbenchmarks.cs.umass.edu/ManyBugs/scenarios/${scenario}.tar.gz" &&\
     tar -xf "${scenario}.tar.gz" &&\
+    ls "${scenario}" &&\
     cp "${scenario}/configuration-default" "$HERE_DIR/${scenario}/configuration-default" &&\
-    cp "${scenario}/diff" "$HERE_DIR/${scenario}" &&\
+    cp -r "${scenario}/diffs" "$HERE_DIR/${scenario}" &&\
     rm -rf "/tmp/${scenario}*" &&\
     popd
   echo "fetched bug: ${scenario}"
 }
+
+for scenario in $(ls -d */); do
+  scenario=${scenario%/}
+  fetch_bug "${scenario}"
+done
