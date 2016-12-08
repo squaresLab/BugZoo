1de83be3d9f4c9d742d5d3c92598740389c61563
Chris Timperley <christimperley@googlemail.com>
2016-12-08 16:51:06 -0500

    fixed typo in manifest assertion

--- sir/pythia/pythia.py
+++ sir/pythia/pythia.py
@@ -32,7 +32,7 @@ class TestManifest(object):
     # loads a manifest from a given file
     def __init__(self, fn):
         assert os.path.isfile(fn), "specified manifest file must exist"
-        assert fn[-4:] == '.json', "specified manifest file must end in .json"
+        assert fn[-5:] == '.json', "specified manifest file must end in .json"
         with open(fn, 'r') as f:
             cases  = json.load(f)
             assert isinstance(cases, list), "manifest file must contain a JSON list"
