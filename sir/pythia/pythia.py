9c349cb0923dfb3bd8e4d2de6985a92bfa1cd09e
Chris Timperley <christimperley@googlemail.com>
2016-12-08 11:55:22 -0500

    progress on better test harness tooling

---
+++ sir/pythia/pythia.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+import click
+import os.path
+import json
+
+# Names of the files that the standard output and standard error from running
+# a given test command should be written to.
+STDOUT_STORAGE_FN = "pythia.out"
+STDERR_STORAGE_FN = "pythia.err"
+
+class TestManifest(object):
+
+    # loads a manifest from a given file
+    def __init__(self, fn):
+        assert os.path.isfile(manifest), "specified manifest file must exist"
+        assert manifest[-4:] == '.json', "specified manifest file must end in .json"
+        with open(fn, 'r') as f:
+            cases  = json.load(f)
+            assert isinstance(cases, list), "manifest file must contain a JSON list"
+            cases = [TestCase.from_json(c) for c in cases]
+            self.__cases = cases
+
+class TestCase(object):
+    def __init__(self, command):
+        self.command = command
+
+    def execute(self, executable, workd):
+        cmd = self.command.replace("<<EXECUTABLE>>", executable)
+        cmd = cmd.replace("<<WORKDIR>>", workd)
+        cmd = "%s > %s 2> %s" % (cmd, STDOUT_STORAGE_FN, STDERR_STORAGE_FN)
+
+# Run 
+
+# Ensures a given directory exists; may encounter a race condition if a
+# directory or file is created between the first and second lines, but that
+# shouldn't happen in this context.
+def ensuredir(d):
+    if not os.path.exists(d):
+        os.makedirs(d)
+
+# Generates the oracle directory for a given problem
+#
+#   generate [manifest] [executable] [inputd] [oracled]
+def generate(manifest, executable, inputd, oracled):
+    assert os.path.isfile(manifest), "specified manifest file must exist"
+    assert manifest[-4:] == '.json', "specified manifest file must end in .json"
+    assert os.path.isfile(executable), "specified executable must exist"
+    assert os.path.isdir(inputd), "specified input directory must exist"
+
+    # Build the oracle directory
+    ensuredir(oracled)
+
+def test_by_num(num, manifest, executable, inputd, oracled):
+    assert os.path.isfile(executable), "specified executable must exist"
+    assert os.path.isdir(inputd), "specified input directory must exist"
+
+    manifest = TestManifest(manifest)
+    case = manifest.getByNum(num)
+
+# run 78
+
+# run a given command and compare against oracle
