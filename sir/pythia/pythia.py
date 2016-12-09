5fd791e5e727b71527f96253209ca61ccc71a366
Chris Timperley <christimperley@googlemail.com>
2016-12-08 22:31:04 -0500

    fixed typo and improved output for run command

--- sir/pythia/pythia.py
+++ sir/pythia/pythia.py
@@ -160,8 +160,8 @@ def generate(args):
 def run(args):
     manifest = TestManifest(args.tests)
     oracle = Oracle.load(args.oracle)
-    test = manifest.get(args.number)
-    print(test.command())
+    test = manifest.get(args.num)
+    print("Running test case #%d: %s", % (args.num, test.command()))
 
 # CLI setup
 PARSER = argparse.ArgumentParser()
