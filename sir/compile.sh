541a49b9c23c7e7ff3fe5bf71620c5566eb99ed9
Chris Timperley <christimperley@googlemail.com>
2017-01-06 12:58:36 -0500

    added generic compile script for SIR

---
+++ sir/compile.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+here_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+src_dir="$here_dir/source"
+
+if [ $# -ne 1 ]; then
+  echo "ERROR: no executable name provided"
+  exit 1
+fi
+
+exe_name=$1
+candidate_dir=$(dirname "$1")
+
+if (!cp "$candidate_dir/*" "$src_dir" -rf ); then
+  echo "ERROR: failed to copy patched files to source directory"
+  exit 1
+fi
+
+if !(make clean && make > /dev/null); then
+  echo "ERROR: failed to execute make within source directory"
+  exit 1
+fi
