3c7acad40199d3c502705733f7cc4d71a8375b80
Chris Timperley <christimperley@googlemail.com>
2017-01-20 21:46:40 -0500

    added Python Makefile

--- many-bugs/python/python-bug-70098-70101/prepare.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ ! -d src ]; then
-  tar -xf src.tar.gz
-  pushd src
-  make clean
-  ./configure --host=i686-pc-linux-gnu --with-dbmliborder=gdbm --with-threads "CFLAGS=-m32 -std=gnu99 -fgnu89-inline" "CXXFLAGS=-m32" "LDFLAGS=-m32"
-  make
-  popd
-fi
