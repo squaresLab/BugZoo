75fce0f4d418ceb24110e216aded6d5ad4ae6de3
Chris Timperley <christimperley@googlemail.com>
2017-02-09 14:08:17 -0500

    finished refactoring PHP bugs

--- manybugs/php/php-bug-2011-03-20-2034e14341-7f2937223d/sanity.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-for (( i=6; i<25; i++ ))
-do
-    $REPAIR configuration-default --seed $i --sample 0.1 --sanity yes --popsize 2 --generations 1
-done
--- manybugs/php/php-bug-2011-03-27-2191af9546-b83e243c23/sanity.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm *cache*
-
-for (( i=0 ; i <25; i++ ))
-do
-    $REPAIR configuration-default --sanity yes --sample 0.1 --popsize 2 --generations 1 --seed $i
-done
