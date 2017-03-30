9d5665c6480e79845155b0eacb3211d0f31c88ae
Chris Timperley <christimperley@googlemail.com>
2017-03-29 22:23:44 -0400

    updated PHP to use shared components

--- ct/openssl/filter.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-while read t; do
-  pushd source/test &> /dev/null
-  perl run_tests.pl "$t" |& grep -q "PASS" && echo "$t"
-  popd &> /dev/null
-done < ${1}
--- ct/php/spin.sh
+++ ct/php/spin.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+docker rm genprog
+docker create --log-driver none \
+              -v /opt/genprog3 \
+              --name genprog \
+              christimperley/genprog
 docker run  --log-driver none \
             --volumes-from genprog \
             --rm \
