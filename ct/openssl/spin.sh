e9bf1d6e356dd3f5b81d73b2fb4c8f171657aa87
Chris Timperley <christimperley@googlemail.com>
2017-03-31 16:05:55 -0400

    updated openssl and added bug one

---
+++ ct/openssl/spin.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+docker rm genprog
+docker create --log-driver none \
+              -v /opt/genprog3 \
+              --name genprog \
+              christimperley/genprog
+docker run  --log-driver none \
+            --volumes-from genprog \
+            --rm \
+            -it christimperley/repairbox:ct-openssl-one
