53f500b32ec3bf202569645e605a7510dc1b9a6a
Chris Timperley <christimperley@googlemail.com>
2017-04-07 14:32:57 -0400

    tweaked Pythia blacklist

--- ct/php/test-by-name.sh
+++ ct/php/test-by-name.sh
@@ -3,5 +3,5 @@ here_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 test_name="$1"
 pushd "${here_dir}/source" > /dev/null
 timeout ${TEST_TIMEOUT} php run-tests.php -q -p sapi/cli/php "${test_name}" \
-  |& grep -q "FAIL " && exit 1
-exit 0
+  |& grep -q "PASS " && exit 0
+exit 1
