d69bbb5b0fdf4e2eb5e76fa0dd8bc6e951dc25c7
ChrisTimperley <christimperley@googlemail.com>
2017-04-19 21:55:30 -0400

    tweaked base setup

---
+++ spin.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+docker rm genprog
+docker create --log-driver none \
+              -v /opt/genprog3 \
+              --name genprog \
+              christimperley/genprog
+docker run  --log-driver none \
+            --volumes-from genprog \
+            --name rbox \
+            --rm \
+            -it christimperley/repairbox:$1
