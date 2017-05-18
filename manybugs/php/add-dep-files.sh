c5c8e508fdaa171d3df392c5ae653f0f69480290
ChrisTimperley <christimperley@googlemail.com>
2017-05-18 17:48:14 -0400

    trash

--- manybugs/php/add-dep-files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-while read -r sc; do
-  sc=$(basename "$sc")
-  #mkdir "${sc}"
-  #cp deps.json "${sc}"
-  cp base.bug.yaml "${sc}.bug.yaml"
-  sed -i "s/BUG_NAME/${sc}/" "${sc}.bug.yaml"
-done < scenarios.txt
