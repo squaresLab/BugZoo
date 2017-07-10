12249508f6d219e19508aadf74ec06d7c1db3da7
ChrisTimperley <christimperley@googlemail.com>
2017-07-10 11:48:58 -0400

    removed valgrind

--- manybugs/valgrind/prepare.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ ! -d valgrind ]; then
-  tar -xvf valgrind.tar.gz
-  cd valgrind
-  aclocal
-  autoconf
-  ./configure --build=i686-linux-gnu "CFLAGS=-m32" "LDFLAGS=-m32" "CXXFLAGS=-m32"
-fi
