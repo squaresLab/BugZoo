be46962204fa71f370630d6626b2210526300b61
ChrisTimperley <christimperley@googlemail.com>
2017-08-28 20:39:48 -0400

    sketching out API

---
+++ repairbox/container.py
@@ -0,0 +1,27 @@
+import docker
+
+
+class BugContainer(object):
+    
+
+    def __init__(self):
+        pass
+
+    
+    def reset(self):
+        """
+        Resets the state of this bug container.
+        """
+        pass
+
+
+    def coverage(self, tests):
+        pass
+
+
+    def compile(self):
+        pass
+
+    
+    def execute(self, tests):
+        pass
