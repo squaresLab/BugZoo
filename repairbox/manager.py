ac87515b38b859996ce33eef794bd7888d325275
ChrisTimperley <christimperley@googlemail.com>
2017-08-30 13:05:05 -0400

    working on manager

--- repairbox/manager.py
+++ repairbox/manager.py
@@ -1,11 +1,17 @@
 import os
+import git
 
 # source update
 
+# source add https://github.com/ChrisTimperley/ManyBugs
 SOURCES = [
     'https://github.com/ChrisTimperley/ManyBugs'
-] # source add https://github.com/ChrisTimperley/ManyBugs
+] 
 
-#PATHS = os.environ.get('REPAIRBOX_MANIFEST_PATH', '')
+# TODO add support for Windows
+DEFAULT_DATA_PATH = \
+    os.path.join(os.environ.get('HOME'), '.repairbox')
+DATA_PATH = os.environ.get('REPAIRBOX_DATA_PATH', DEFAULT_DATA_PATH)
 
 
+# download the repo
