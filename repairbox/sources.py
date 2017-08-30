e9bb1dce31bf32dedde2658ed70a52e80008ab3b
ChrisTimperley <christimperley@googlemail.com>
2017-08-30 15:06:24 -0400

    more work on source manager

--- repairbox/sources.py
+++ repairbox/sources.py
@@ -1,4 +1,6 @@
 import git
+import json
+
 
 class SourceManager(object):
     """
@@ -9,12 +11,12 @@ class SourceManager(object):
         __sources (dict of str to str):
     """
     
-
-    def __init__(self, rbox: RepairBox) -> None:
-        self.__rbox: str = rbox
-        self.__sources_filename: str = os.path.join(rbox.path(), 'sources.json')
-        self.__source_path = os.path.join(rbox.path(), 'sources')
-        self.__sources: dict = {}
+   # def __init__(self, rbox: RepairBox) -> None:
+   #     self.__rbox: str = rbox
+    def __init__(self, path: str) -> None:
+        self.__sources_filename: str = os.path.join(path, 'sources.json') # TODO: update
+        self.__source_path = os.path.join(path, 'sources')
+        self.reload()
 
 
     def __source_rel_path(self, src: str) -> str:
@@ -23,6 +25,22 @@ class SourceManager(object):
     
     def __source_abs_path(self, src: str) -> str:
         return os.path.join(self.__source_path, self.__source_rel_path(src))
+
+
+    def reload(self) -> None:
+        if not os.path.exists(self.__sources_filename):
+            self.__sources = {}
+            return
+
+        with open(self.__sources_filename 'r') as f:
+            srcs = json.load(f)
+
+        assert isinstance(srcs, dict)
+        assert all(type(k) in [str, unicode] for k in srcs)
+        assert all(type(v) in [str, unicode] for v in srcs)
+
+        self.__sources = srcs
+        
 
 
     def add(self, src: str) -> None:
@@ -39,12 +57,25 @@ class SourceManager(object):
     
 
     def remove(self, src: str) -> None:
-        pass
+        assert src != ""
+        if src not in self.__sources:
+            raise Exception("source not found: {}".format(src)) # TODO custom Error
+
+
+    def update_source(self, src: str) -> None:
+        """
+        Update the files for a given source.
+        """
 
 
     def update(self) -> None:
-        pass
+        for src in self.__sources:
+            self.update_source(src)
 
 
     def sources(self) -> List[str]:
         return list(self.__sources.keys())
+
+
+if __name__ == "__main__":
+    srcMgr = SourceManager("/home/chris/.repairbox")
