05338d726fe36ded7fdced908bb31e4f2cd918cd
ChrisTimperley <christimperley@googlemail.com>
2017-11-07 18:48:03 -0500

    added Tool.build

--- repairbox/tool.py
+++ repairbox/tool.py
@@ -2,6 +2,7 @@ import yaml
 import os
 from repairbox.build import BuildInstructions
 
+
 class Tool(object):
     @staticmethod
     def from_file(fn: str) -> 'Tool':
@@ -36,6 +37,17 @@ class Tool(object):
         The name of this tool.
         """
         return self.__name
+
+
+    def build(self, force=False) -> None:
+        """
+        Constructs the Docker image associated with this tool. By default, this
+        command will do nothing if a version of the image associated with the
+        tool is already installed to the local machine. However, if `force` is
+        set to `True`, the Docker image will be rebuilt from scratch, replacing
+        any exiting image.
+        """
+        return self.__build_instructions.build(force=force)
 
 
 class ToolManager(object):
