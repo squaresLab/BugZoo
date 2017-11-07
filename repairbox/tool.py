5d417b0b64d9340f8287f6a7612fe012118fca07
ChrisTimperley <christimperley@googlemail.com>
2017-11-07 18:23:38 -0500

    added more methods to tool module

--- repairbox/tool.py
+++ repairbox/tool.py
@@ -1,11 +1,51 @@
+from repairbox.build import BuildInstructions
+
 class Tool(object):
-    def __init__(self, name):
+    @staticmethod
+    def from_file(fn: str) -> 'Tool':
+        """
+        Loads a tool from its YAML manifest file.
+        """
+        with open(fn, 'r') as f:
+            yml = yaml.load(f)
+
+        assert 'name' in yml
+        name = yml['name']
+
+        assert 'docker' in yml
+        build_instructions = BuildInstructions.from_dict(yml['docker'])
+
+        return Tool(name, build_instructions)
+
+
+    def __init__(self,
+                 name: str,
+                 build_instructions: BuildInstructions) -> None:
         self.__name = name
+        self.__build_instructions = build_instructions
+
+
+    @property
+    def name(self) -> 'str':
+        """
+        The name of this tool.
+        """
+        return self.__name
 
 
 class ToolManager(object):
     def __init__(self):
         self.__tools = {}
+        self.scan()
+
+
+    def scan(self):
+        """
+        Scans the local RepairBox installation for installed tools.
+        """
+        fn = "/home/chris/git/genprog/genprog.tool.rbox.yml"
+        tool = Tool.from_file(fn)
+        self.__tools['genprog'] = tool
 
 
     def __getitem__(name: str) -> Tool:
