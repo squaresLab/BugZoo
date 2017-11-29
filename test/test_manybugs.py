50da34d83dfaa8d992e7b3ba348ae1aa7fd338b3
ChrisTimperley <christimperley@googlemail.com>
2017-11-29 17:23:44 -0500

    updated manybugs test

--- test/test_manybugs.py
+++ test/test_manybugs.py
@@ -16,20 +16,33 @@ class CLITestCase(unittest.TestCase):
 
 class ManyBugsTestCase(CLITestCase):
     def test_add_source(self):
+        bz = bugzoo.BugZoo()
         manybugs_url = 'https://github.com/ChrisTimperley/ManyBugs'
 
         # attempt to install ManyBugs
         self.run_command(['source', 'add', manybugs_url])
 
         # check that the source has been registered
-        bz = bugzoo.BugZoo()
         try:
+            bz.rescan()
             bz.sources.get_by_url(manybugs_url)
         except:
             self.fail("Failed to find registered source.")
 
         # remove the source
         self.run_command(['source', 'remove', manybugs_url])
+
+        bz.rescan()
+        with self.assertRaises(bugzoo.errors.SourceNotFoundWithURL):
+            bz.sources.get_by_url(manybugs_url)
+
+        # re-add ManyBugs!
+        self.run_command(['source', 'add', manybugs_url])
+        try:
+            bz.rescan()
+            bz.sources.get_by_url(manybugs_url)
+        except:
+            self.fail("Failed to find registered source.")
 
 
 if __name__ == '__main__':
