c4cc163d2488e78b790522b2f3f26f00074e30c7
ChrisTimperley <christimperley@googlemail.com>
2017-12-21 12:40:56 +0800

    added extra imports to __init__.py

--- bugzoo/__init__.py
+++ bugzoo/__init__.py
@@ -2,6 +2,7 @@ from bugzoo.manager import BugZoo
 import bugzoo.cli
 import bugzoo.tool
 
+from bugzoo.container import Container
 from bugzoo.patch import Patch
 from bugzoo.bug import Bug
 from bugzoo.coverage import ProjectLineCoverage, FileLineCoverage
