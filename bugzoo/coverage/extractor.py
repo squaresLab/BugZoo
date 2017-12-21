113e114c18eb7764e12d5c19ed7f130077f87659
ChrisTimperley <christimperley@googlemail.com>
2017-12-21 12:24:38 +0800

    begun implementing coverage extractor for C and C++

--- bugzoo/coverage/extractor.py
+++ bugzoo/coverage/extractor.py
@@ -36,3 +36,22 @@ class CoverageExtractor(object):
         self._prepare(container)
         container.execute(test)
         return self._extract(container)
+
+
+class CCoverageExtractor(object):
+    """
+    Responsible for collecting coverage information from programs written in C
+    and C++.
+    """
+    @override
+    def _prepare(self,
+                 container: Container
+                 ) -> None:
+        options = {
+            "CFLAGS": "CFLAGS='-fprofile-arcs -ftest-coverage -fPIC'"
+        }
+
+        outcome = container.compile(options=options)
+        if not outcome.passed:
+            msg = "failed to generate coverage for container ({}) due to compilation failure.".format(container.id)
+            raise Exception(msg)
