20a58e4f220d3d9f8a1bda91d52ebb6f1ec6327e
ChrisTimperley <christimperley@googlemail.com>
2017-12-24 11:44:59 +0800

    moved exec response to cmd module

--- bugzoo/core.py
+++ bugzoo/core.py
@@ -1,7 +1,6 @@
 from bugzoo.coverage import CoverageExtractor, \
                             CCoverageExtractor
 import enum
-import docker
 
 
 class LanguageEnumMeta(enum.EnumMeta):
@@ -25,74 +24,3 @@ class Language(enum.Enum, metaclass=LanguageEnumMeta):
         for programs written in this language.
         """
         return self.__coverage_extractor
-
-
-class PendingExecResponse(object):
-    def __init__(self, exec_response, output) -> None:
-        self.__exec_response = exec_response
-        self.__output = output
-
-    @property
-    def running(self) -> bool:
-        client = docker.from_env()
-        return client.api.exec_inspect(self.exec_response)['Running']
-
-    @property
-    def exec_response(self):
-        return self.__exec_response
-
-    @property
-    def output(self):
-        return self.__output
-
-    @property
-    def exit_code(self) -> int:
-        if self.running:
-            return None
-        client = docker.from_env()
-        id = self.exec_response['Id']
-        return client.api.exec_inspect(id)
-
-
-class ExecResponse(object):
-    """
-    Used to hold the response from a command execution.
-    """
-    def __init__(self,
-                 code: int,
-                 duration: float,
-                 output: str
-                 ) -> None:
-        self.__code = code
-        self.__duration = duration
-        self.__output = output
-
-    @property
-    def code(self) -> int:
-        """
-        The exit code of the execution.
-        """
-        return self.__code
-
-    @property
-    def duration(self) -> float:
-        """
-        The length of time taken to complete the execution.
-        """
-        return self.__duration
-
-    @property
-    def output(self) -> str:
-        """
-        The output of the execution.
-        """
-        return self.__output.decode(sys.stdout.encoding)
-
-    def to_dict(self) -> dict:
-        """
-        Produces a JSON and YAML--ready dictionary description of this
-        command execution response.
-        """
-        return {'code': self.code,
-                'duration': self.duration,
-                'output': self.output}
