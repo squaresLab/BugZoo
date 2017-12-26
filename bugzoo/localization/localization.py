2b713f1a451cd21964461b5280ab50a381c707a9
ChrisTimperley <christimperley@googlemail.com>
2017-12-26 15:06:39 +0800

    added 'restricted_to_files' method to Localization

--- bugzoo/localization/localization.py
+++ bugzoo/localization/localization.py
@@ -23,12 +23,23 @@ class Localization(object):
     def __init__(self,
                  scores: Dict[FileLine, float]
                  ) -> None:
-        self.__scores = copy(scores)
+        self.__scores = {l: s for (l, s) in scores.items() if s > 0}
 
         sum_scores = float(sum(self.__scores.values()))
 
         self.__normalized = \
-            {line: s/sum_scores for (line, s) in scores.items()}
+            {line: s/sum_scores for (line, s) in self.__scores.items()}
+
+    def restricted_to_files(self,
+                            filenames: List[str]
+                            ) -> 'Localization':
+        """
+        Returns a variant of this fault localization that only contains entries
+        for lines that belong to any of the specified files.
+        """
+        scores = {l: s for (l, s) in self.__scores.items() \
+                  if l.filename in filenames}
+        return Localization(scores)
 
     def score(self, line: FileLine) -> float:
         """
