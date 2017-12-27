45b9cb74891dd8dd5e6d5f3d9afe8e225110868b
ChrisTimperley <christimperley@googlemail.com>
2017-12-27 11:21:33 +0800

    modified Hunk to print complete headers

--- bugzoo/patch.py
+++ bugzoo/patch.py
@@ -124,8 +124,20 @@ class Hunk(object):
         """
         Returns the contents of this hunk as part of a unified format diff.
         """
-        header = '@@ -{} +{} @@'.format(self.__old_start_at,
-                                        self.__new_start_at)
+        num_deleted = \
+            sum(1 for l in self.__lines if isinstance(l, DeletedLine))
+        num_inserted = \
+            sum(1 for l in self.__lines if isinstance(l, InsertedLine))
+        num_context = \
+            sum(1 for l in self.__lines if isinstance(l, ContextLine))
+
+        num_old_lines = num_context + num_deleted
+        num_new_lines = num_context + num_inserted
+
+        header = '@@ -{},{} +{},{} @@'.format(self.__old_start_at,
+                                              num_old_lines,
+                                              self.__new_start_at,
+                                              num_new_lines)
         body = [str(line) for line in self.__lines]
         return '\n'.join([header] + body)
 
