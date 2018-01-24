b925c6c33ef04e4ac88a8d34faebd19d577c418d
ChrisTimperley <christimperley@googlemail.com>
2018-01-24 06:09:58 -0500

    added error codes for server

---
+++ bugzoo/server/code.py
@@ -0,0 +1,23 @@
+from typing import Tuple, Dict
+from enum import Enum
+
+
+class ErrorCode(Enum):
+    BUG_NOT_FOUND = ("No bug was found with the given UID.", 404)
+    BUG_NOT_SPECIFIED = ("No bug ID was specified.", 400)
+
+    def __init__(self, message: str, status_code: int) -> None:
+        self.__message = message
+        self.__status_code = status_code
+
+    @property
+    def message(self) -> str:
+        return self.__message
+
+    @property
+    def status_code(self) -> int:
+        return self.__status_code
+
+    def to_response(self) -> Tuple[Dict[str, str], int]:
+        jsn = {'error': {'message': self.message}}
+        return (jsn, self.status_code)
