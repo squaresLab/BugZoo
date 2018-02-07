103b54106afff28f6579b5c4beaece33d920ce1c
ChrisTimperley <christimperley@googlemail.com>
2018-02-07 17:01:10 -0500

    cosmetic: whitespace tidying

--- bugzoo/source.py
+++ bugzoo/source.py
@@ -22,7 +22,6 @@ class Source(object):
         # TODO
         raise "UNEXPECTED SOURCE TYPE"
 
-
     @staticmethod
     def url_to_rel_path(url: str) -> str:
         rel_path = url.replace('https://', '')
@@ -30,12 +29,10 @@ class Source(object):
         rel_path = rel_path.replace('.', '_')
         return rel_path
 
-
     @staticmethod
     def url_to_abs_path(manager: 'SourceManager', url: str) -> str:
         rel_path = Source.url_to_rel_path(url)
         return os.path.join(manager.path, rel_path)
-
 
     def __init__(self,
                  manager: 'SourceManager',
@@ -46,16 +43,13 @@ class Source(object):
         self.__name = name
         self.__repo = git.Repo(self.abs_path)
 
-
     @property
     def manifest_fn(self) -> str:
         return os.path.join(self.abs_path, '.bugzoo.yml')
 
-
     @property
     def name(self) -> str:
         return self.__name
-
 
     @property
     def version(self) -> str:
@@ -66,30 +60,24 @@ class Source(object):
         sha = self.__repo.head.object.hexsha
         return self.__repo.git.rev_parse(sha, short=8)
 
-
     def update(self) -> None:
         origin = self.__repo.remotes.origin
         origin.pull()
 
-
     def remove(self) -> None:
         shutil.rmtree(self.abs_path)
-
 
     @property
     def manager(self):
         return self.__manager
 
-
     @property
     def url(self) -> str:
         return self.__url
 
-
     @property
     def rel_path(self) -> str:
         return Source.url_to_rel_path(self.url)
-
 
     @property
     def abs_path(self) -> str:
