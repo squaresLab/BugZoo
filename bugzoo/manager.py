c4dfbec1ae426f78a10f545e4790a76957fc679d
ChrisTimperley <christimperley@googlemail.com>
2018-03-08 15:31:10 -0500

    refactoring: moved BugManager to manager module

--- bugzoo/manager.py
+++ bugzoo/manager.py
@@ -2,9 +2,9 @@ from typing import Iterator
 import os
 
 from .source import SourceManager
-from .bug import Bug
 from .dataset import Dataset
 from .mgr.tool import ToolManager
+from .mgr.bug import BugManager
 
 
 class BugZoo(object):
@@ -57,7 +57,7 @@ class BugZoo(object):
         self.__sources.scan()
 
     @property
-    def sources(self):
+    def sources(self) -> SourceManager:
         """
         The sources registered with this BugZoo installation.
         """
@@ -71,14 +71,14 @@ class BugZoo(object):
         return self.__datasets
 
     @property
-    def tools(self):
+    def tools(self) -> ToolManager:
         """
         The tools registered with this BugZoo installation.
         """
         return self.__tools
 
     @property
-    def bugs(self):
+    def bugs(self) -> BugManager:
         """
         The bugs registered with this BugZoo installation.
         """
@@ -99,35 +99,3 @@ class DatasetManager(object):
             if dataset.name == name_or_url or dataset.url == name_or_url:
                 return dataset
         raise IndexError
-
-
-class BugManager(object):
-    """
-    Used to access and manage all bugs registered with a local BugZoo
-    installation.
-    """
-    class BugIterator(object):
-        def __init__(self, datasets):
-            self.__datasets = [d for d in datasets]
-            self.__bugs = []
-
-        def __next__(self):
-            if not self.__bugs:
-                if not self.__datasets:
-                    raise StopIteration
-                src = self.__datasets.pop()
-                self.__bugs += src.bugs
-                return self.__next__()
-            return self.__bugs.pop()
-
-    def __init__(self, installation: 'BugZoo'):
-        self.__installation = installation
-
-    def __getitem__(self, name: str) -> Bug:
-        for src in self.__installation.datasets:
-            if src.contains(name):
-                return src[name]
-        raise IndexError('bug not found: {}'.format(name))
-
-    def __iter__(self) -> Iterator[Bug]:
-        return Bugs.BugIterator(self.__installation.datasets)
---
+++ bugzoo/mgr/bug.py
@@ -0,0 +1,35 @@
+from typing import Iterator
+
+from ..bug import Bug
+
+
+class BugManager(object):
+    """
+    Used to access and manage all bugs registered with a local BugZoo
+    installation.
+    """
+    class BugIterator(object):
+        def __init__(self, datasets):
+            self.__datasets = [d for d in datasets]
+            self.__bugs = []
+
+        def __next__(self):
+            if not self.__bugs:
+                if not self.__datasets:
+                    raise StopIteration
+                src = self.__datasets.pop()
+                self.__bugs += src.bugs
+                return self.__next__()
+            return self.__bugs.pop()
+
+    def __init__(self, installation: 'BugZoo'):
+        self.__installation = installation
+
+    def __getitem__(self, name: str) -> Bug:
+        for src in self.__installation.datasets:
+            if src.contains(name):
+                return src[name]
+        raise IndexError('bug not found: {}'.format(name))
+
+    def __iter__(self) -> Iterator[Bug]:
+        return Bugs.BugIterator(self.__installation.datasets)
