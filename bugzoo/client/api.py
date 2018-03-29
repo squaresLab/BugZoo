93ecb9bba0f221adcb143746efa57ab8598615b4
ChrisTimperley <christimperley@googlemail.com>
2018-03-29 00:51:26 -0400

    added APIClient class

--- bugzoo/client/api.py
+++ bugzoo/client/api.py
@@ -1,17 +1,22 @@
-# class APIClient(object):
-#     def __init__(self, base_url: str):
-#         self.__base_url = base_url
-# 
-#     def _url(self, path: str) -> str:
-#         """
-#         Computes the URL for a resource located at a given path on the server.
-#         """
-# 
-# 
-#     def _get(self, path: str) -> 
-# 
-#     def tool_upload(self, tool_name: str) -> None:
-#         pass
-# 
-#     def tool_download(self, tool_name: str) -> None:
-#         pass
+from typing import List
+import requests
+import urllib.parse
+
+
+class APIClient(object):
+    def __init__(self, base_url: str):
+        self.__base_url = base_url
+
+    def _url(self, path_components: List[str]) -> str:
+        """
+        Computes the URL for a resource located at a given path on the server.
+        """
+        return urllib.parse.urljoin(self.__base_url, *path_components)
+
+    def get(self, path_components: List[str]) -> requests.Response:
+        url = self._url(path_components)
+        return requests.get(url)
+
+    def post(self, path_components: List[str]) -> requests.Response:
+        url = self._url(path_components)
+        return requests.post(url)
