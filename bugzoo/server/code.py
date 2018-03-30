d27cad1dd8c7a93eb7ce4162d9cb27a054c0831c
ChrisTimperley <christimperley@googlemail.com>
2018-03-29 23:44:29 -0400

    added TEST_NOT_FOUND to error codes

--- bugzoo/server/code.py
+++ bugzoo/server/code.py
@@ -8,9 +8,11 @@ class ErrorCode(Enum):
     BUG_NOT_SPECIFIED = ("No bug ID was specified.", 400)
     BUG_ALREADY_INSTALLED = ("Bug has already been installed.", 200)
     CONTAINER_NOT_FOUND = ("No (running) container was found with the given UID.", 404)
+    TEST_NOT_FOUND = ("No test was found with the given identifier.", 404)
     IMAGE_BUILD_FAILED = ("Failed to build image.", 500)
     IMAGE_NOT_INSTALLED = ("Image not installed.", 500)
     FAILED_TO_COMPUTE_COVERAGE = ("Failed to compute coverage.", 500)
+    COMMAND_NOT_SPECIFIED = ("No command was specified.", 422)
 
     def __init__(self, message: str, status_code: int) -> None:
         self.__message = message
