524e8cd6414377fed2a48344526531c4abdb494b
ChrisTimperley <christimperley@googlemail.com>
2018-04-19 19:28:32 -0400

    fixed typing issues in client errors module

--- bugzoo/client/errors.py
+++ bugzoo/client/errors.py
@@ -1,13 +1,16 @@
-class UnexpectedAPIResponse(object):
+import flask
+
+
+class UnexpectedAPIResponse(Exception):
     """
     The BugZoo API produced a response that was unexpected by the client.
     """
-    def __init__(self, response):
+    def __init__(self, response: flask.Response) -> None:
         msg = "Unexpected API response [{}]: {}"
         msg = msg.format(response.status_code, response.text)
         super().__init__(msg)
         self.__response = response
 
     @property
-    def response(self):
+    def response(self) -> flask.Response:
         return self.__response
