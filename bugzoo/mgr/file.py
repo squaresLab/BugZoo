8879dd42426a8b2203b25a74d2c43c36b575e9a4
ChrisTimperley <christimperley@googlemail.com>
2018-04-20 15:18:50 -0400

    added stub write method to server-side file manager

--- bugzoo/mgr/file.py
+++ bugzoo/mgr/file.py
@@ -15,7 +15,10 @@ class FileManager(object):
         self.__mgr_bug = mgr_bug
         self.__mgr_ctr = mgr_ctr
 
-    def read(self, container: Container, filepath: str) -> str:
+    def read(self,
+             container: Container,
+             filepath: str
+             ) -> str:
         bug = self.__mgr_bug[container.bug]
 
         if not os.path.isabs(filepath):
@@ -41,4 +44,11 @@ class FileManager(object):
                filepath: str,
                force: bool = False
                ) -> None:
+        raise NotImplementedError
+
+    def write(self,
+              container: Container,
+              filepath: str,
+              contents: str
+              ) -> str:
         raise NotImplementedError
