a574590d3c2f884e35b1065dc89808a4c3340e0c
ChrisTimperley <christimperley@googlemail.com>
2018-04-23 21:24:17 -0400

    added __all__ to client module and added Client to __init__.py for bugzoo

--- bugzoo/__init__.py
+++ bugzoo/__init__.py
@@ -1,4 +1,5 @@
 from bugzoo.version import __version__
+from bugzoo.client import Client
 from bugzoo.manager import BugZoo
 from bugzoo.core import Language, Patch
 from bugzoo.core.container import Container
