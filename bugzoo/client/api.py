02c698686328e29f19d74118903a00fa088363c1
ChrisTimperley <christimperley@googlemail.com>
2018-04-23 22:39:23 -0400

    added handle_erroneous_response method to APIClient

--- bugzoo/client/api.py
+++ bugzoo/client/api.py
@@ -4,6 +4,8 @@ import logging
 import requests
 import urllib.parse
 
+from ..exceptions import *
+
 
 class APIClient(object):
     def __init__(self, base_url: str) -> None:
@@ -16,6 +18,22 @@ class APIClient(object):
         Computes the URL for a resource located at a given path on the server.
         """
         return urllib.parse.urljoin(self.__base_url, path)
+
+    def handle_erroneous_response(self, response: requests.Response) -> None:
+        """
+        Attempts to decode an erroneous response into an exception, and to
+        subsequently throw that exception.
+
+        Raises:
+            BugZooException: the exception described by the error response.
+            UnexpectedResponse: if the response cannot be decoded to an
+                exception.
+        """
+        try:
+            err = BugZooException.from_dict(response.json())
+        except Exception:
+            err = UnexpectedResponse(response)
+        raise err
 
     def get(self,
             path: str,
