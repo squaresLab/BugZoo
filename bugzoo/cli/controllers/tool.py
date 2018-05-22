de2547765154cc2f42d50cac06bb4e37f441aabf
ChrisTimperley <christimperley@googlemail.com>
2018-05-22 10:45:18 -0400

    ported tool commands to cement CLI

---
+++ bugzoo/cli/controllers/tool.py
@@ -0,0 +1,87 @@
+import operator
+
+from cement.ext.ext_argparse import ArgparseController, expose
+
+
+class ToolController(ArgparseController):
+    class Meta:
+        label = 'tool'
+        description = 'install and remove tools'
+        stacked_on = 'base'
+        stacked_type = 'nested'
+        output_handler = 'tabulate'
+        extensions = ['tabulate']
+
+    @expose(
+        help='produces a list of registered tools',
+        arguments=[
+            (['-q'], {'help': 'prints an unannotated list of the names of all registered tools',  # noqa: pycodestyle
+                      'action': 'store_true'}),
+        ]
+    )
+    def list(self) -> None:
+        headers = ['Tool', 'Source', 'Installed?']
+        data = []
+        tools = self.app.daemon.tools
+        for tool in tools:
+            row = [
+                tool.name,
+                tool.source if tool.source else '-',
+                'Yes' if tools.is_installed(tool) else 'No'
+            ]
+            data.append(row)
+        data = sorted(data, key=operator.itemgetter(1, 0))
+        if self.app.pargs.q:
+            print('\n'.join([r[0] for r in data]))
+        else:
+            self.app.render(data, headers=headers)
+
+    @expose(
+        help='downloads the Docker image for a given tool',
+        arguments=[(['tool'], {'help': 'the name of the tool', 'type': str})]
+    )
+    def download(self) -> None:
+        name_tool = self.app.pargs.tool  # type: str
+        print('downloading tool: {}'.format(name_tool))
+        tools = self.app.daemon.tools
+        tool = tools[name_tool]
+        tools.download(tool)
+
+    @expose(
+        help='uploads the Docker image for a given tool',
+        arguments=[(['tool'], {'help': 'the name of the tool', 'type': str})]
+    )
+    def upload(self) -> None:
+        name_tool = self.app.pargs.tool  # type: str
+        print('uploading tool: {}'.format(name_tool))
+        tools = self.app.daemon.tools
+        tool = tools[name_tool]
+        tools.upload(tool)
+
+    @expose(
+        help='builds the Docker image for a given bug',
+        arguments=[(['tool'], {'help': 'the name of the tool', 'type': str})]
+    )
+    def build(self) -> None:
+        name_tool = self.app.pargs.tool  # type: str
+        print('building tool: {}'.format(name_tool))
+        tools = self.app.daemon.tools
+        tool = tools[name_tool]
+        tools.build(tool)
+
+    @expose(
+        help='uninstalls the Docker image for a given bug',
+        arguments=[
+            (['tool'], {'help': 'the name of the tool', 'type': str}),
+            (['-f', '--force'],
+             {'help': 'does not report an error if image is not installed.',
+              'action': 'store_true'})
+        ]
+    )
+    def uninstall(self) -> None:
+        force = self.app.pargs.force  # type: bool
+        name_tool = self.app.pargs.tool  # type: str
+        print('uninstalling tool: {}'.format(name_tool))
+        tools = self.app.daemon.tools
+        tool = tools[name_tool]
+        tools.uninstall(tool, force=force)
