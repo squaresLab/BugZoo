1b7e55ed09b6b3cbd7201439c124e4e579a7d272
Chris Timperley <christimperley@googlemail.com>
2018-06-10 15:57:46 -0400

    Added instrument parameter to coverage methods (#224)
    * added rebuild argument to client container coverage method
    
    * added rebuild arg to server
    
    * implemented instrument flag in container coverage method

--- bugzoo/server/__init__.py
+++ bugzoo/server/__init__.py
@@ -227,8 +227,13 @@ def coverage_container(id_container: str):
     except KeyError:
         return BugNotFound(container.bug), 500
 
+    instrument = \
+        flask.request.args.get('instrument', 'True').lower() == 'yes'
+
     try:
-        coverage = daemon.containers.coverage(container)
+        # FIXME implement
+        coverage = daemon.containers.coverage(container,
+                                              instrument=instrument)
     except Exception as err:
         logger.exception("failed to compute coverage for container [%s]: %s",
                      id_container, err)
