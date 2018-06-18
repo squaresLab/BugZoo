a9c2abbf3770d68df85e36e0223ad036ad60d6b0
ChrisTimperley <christimperley@googlemail.com>
2018-06-18 14:08:23 -0400

    removed old testing module

--- bugzoo/testing/__init__.py
+++
@@ -1,3 +0,0 @@
-from bugzoo.testing.base import TestSuite, TestCase, TestOutcome
-from bugzoo.testing.simple import SimpleTestSuite, GenProgTestSuite
-from bugzoo.testing.empty import EmptyTestSuite
