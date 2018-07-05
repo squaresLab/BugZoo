cd4cab5dfd466228d35e8b6c02399b191fb345f9
ChrisTimperley <christimperley@googlemail.com>
2018-07-05 12:34:18 -0400

    don't report free memory

--- bugzoo/util.py
+++ bugzoo/util.py
@@ -22,9 +22,7 @@ def report_system_resources(logger: logging.Logger) -> None:
     cores_logical = psutil.cpu_count(logical=True)
     cpu_freq = psutil.cpu_freq().max
     vmem_total = bytes_to_gigabytes(psutil.virtual_memory().total)
-    vmem_free = bytes_to_gigabytes(psutil.virtual_memory().free)
     swap_total = bytes_to_gigabytes(psutil.swap_memory().total)
-    swap_free = bytes_to_gigabytes(psutil.swap_memory().free)
     disk_info = psutil.disk_usage('/')
     disk_size = bytes_to_gigabytes(disk_info.total)
     disk_free = bytes_to_gigabytes(disk_info.free)
@@ -32,7 +30,7 @@ def report_system_resources(logger: logging.Logger) -> None:
     resource_s = '\n'.join([
         '* CPU cores: {} physical, {} logical'.format(cores_physical, cores_logical),  # noqa: pycodestyle
         '* CPU frequency: {:.2f} GHz'.format(cpu_freq / 1000),
-        '* virtual memory: {:.2f} GB ({:.2f} GB free)'.format(vmem_total, vmem_free),  # noqa: pycodestyle
+        '* virtual memory: {:.2f} GB'.format(vmem_total),
         '* swap memory: {:.2f} GB ({:.2f} GB free)'.format(swap_total, swap_free),  # noqa: pycodestyle
         '* disk space: {:.2f} GB ({:.2f} GB free)'.format(disk_size, disk_free)  # noqa: pycodestyle
     ])
