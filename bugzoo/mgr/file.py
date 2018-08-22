8478b321476510c673959eb50b220803b68566b0
ChrisTimperley <christimperley@googlemail.com>
2018-08-22 15:09:52 -0400

    added character encoding detection to FileManager.read

--- bugzoo/mgr/file.py
+++ bugzoo/mgr/file.py
@@ -1,10 +1,21 @@
+"""
+This module provides functionality that allows users to interact with container
+filesystems (e.g., reading, writing, and deleting files).
+"""
+__all__ = ['FileManager']
+
 import os
 import logging
 import tempfile
 
+import chardet
+
 from .bug import BugManager
 from .container import ContainerManager
 from ..core.container import Container
+
+logger = logging.getLogger(__name__)  # type: logging.Logger
+logger.setLevel(logging.DEBUG)
 
 
 class FileManager(object):
@@ -19,14 +30,21 @@ class FileManager(object):
              container: Container,
              filepath: str
              ) -> str:
+        """
+        Reads the contents of a given file belonging to a container.
+        """
+        logger.debug("reading contents of file [%s] inside container [%s]",
+                     filepath, container.id)
         bug = self.__mgr_bug[container.bug]
+        filepath_orig = filepath
 
         if not os.path.isabs(filepath):
             filepath = os.path.join(bug.source_dir, filepath)
+            logger.debug("converted relative path to absolute path: %s -> %s",
+                         filepath_orig, filepath)
 
         # TODO what if the file doesn't exist?
         # TODO what if we encounter a permissions problem?
-        # TODO what if we can't decode the file?
 
         # copy the contents the file to a temporary file on the host machine
         # TODO ensure correct permissions
@@ -34,10 +52,35 @@ class FileManager(object):
         try:
             # TODO we can probably move 'copy_from' to this manager
             self.__mgr_ctr.copy_from(container, filepath, fn_host)
-            with open(fn_host, 'r') as fh:
-                return fh.read()
+            with open(fn_host, 'rb') as fh:
+                blob = fh.read()
         finally:
             os.remove(fn_host)
+
+        # detect encoding
+        logger.debug("detecting encoding for file [%s] in container [%s]",
+                     filepath_orig, container.id)
+        chardet_res = chardet.detect(blob)
+        encoding = chardet_res['encoding']
+        confidence = chardet_res['confidence']
+        logger.debug("detected encoding of file [%s] in container [%s]: %s (%.3f confidence)",  # noqa: pycodestyle
+                     filepath_orig, container.id, encoding, confidence)
+
+        # decode file
+        logger.debug("decoding file [%s] in container [%s]",
+                     filepath_orig, container.id)
+        try:
+            contents = blob.decode(encoding)
+        except UnicodeDecodeError:
+            logger.exception("failed to decode contents of file [%s] in container [%s]",  # noqa: pycodestyle
+                             filepath_orig, container.id)
+            raise
+        logger.debug("decoded file [%s] in container [%s]",
+                     filepath_orig, container.id)
+
+        logger.debug("read contents of file [%s] inside container [%s]",
+                     filepath_orig, container.id)
+        return contents
 
     def delete(self,
                container: Container,
