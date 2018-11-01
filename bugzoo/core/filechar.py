9918ce78c0245e03f17064e0eb8c91b7978e4ccd
ChrisTimperley <christimperley@googlemail.com>
2018-11-01 13:21:28 -0400

    refactored filechar module

--- bugzoo/core/filechar.py
+++ bugzoo/core/filechar.py
@@ -1,42 +1,35 @@
+__all__ = ['FileChar', 'FileCharRange']
+
+import attr
+
+
+@attr.s(frozen=True)
 class FileChar(object):
     """
     Encodes the location of a given character in a source code file.
     """
-    def __init__(self,
-                 filename: str,
-                 offset: int
-                 ) -> None:
-        self.__filename = filename
-        self.__offset = offset
+
+    """The name of the file to which this character belongs."""
+    filename = attr.ib(type=str)
+
+    """The number of characters that precede this character in the file."""
+    offset = attr.ib(type=int)
 
     def __str__(self) -> str:
         return "{}[{}]".format(self.filename, self.offset)
 
-    @property
-    def filename(self) -> str:
-        """
-        The name of the file to which this character belongs.
-        """
-        return self.__filename
 
-    @property
-    def offset(self) -> int:
-        """
-        The number of characters that precede this character in the file.
-        """
-        return self.__offset
-
-
+@attr.s(frozen=True)
 class FileCharRange(object):
     """
     Describes a contiguous range of characters in a single file.
     """
-    def __init__(self,
-                 start: FileChar,
-                 stop: FileChar
-                 ) -> None:
-        self.__start = start
-        self.__stop = stop
+
+    """The location of the character at which this range begins."""
+    start = attr.ib(type=FileChar)
+
+    """The location of the character at which this range ends."""
+    stop = attr.ib(type=FileChar)
 
     def __str__(self) -> str:
         return "{}[{}..{}]".format(self.filename,
@@ -49,22 +42,6 @@ class FileCharRange(object):
         The name of the file that this character range covers.
         """
         return self.start.filename
-
-    @property
-    def start(self) -> FileChar:
-        """
-        The location of the character at which this range begins.
-        """
-        return self.__start
-
-    @property
-    def stop(self) -> FileChar:
-        """
-        The location of the character at which this range ends.
-
-        TODO inclusive?
-        """
-        return self.__stop
 
     def __len__(self) -> int:
         """
