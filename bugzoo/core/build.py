24fafec2de7b8f7892eb1c30a54cea95bed6ca96
ChrisTimperley <christimperley@googlemail.com>
2018-11-01 13:09:45 -0400

    updated build module

--- bugzoo/core/build.py
+++ bugzoo/core/build.py
@@ -1,3 +1,5 @@
+__all__ = ['BuildInstructions']
+
 from typing import Optional, Dict
 from pprint import pprint as pp
 import warnings
@@ -5,61 +7,55 @@ import os
 import shutil
 import json
 
+import attr
 import docker
 import yaml
 
 
+def converter_args(args: Dict[str, Any]) -> Dict[str, str]:
+    return {k: str(v) for (k, v) in args.items()}
+
+
+@attr.s(frozen=True)
 class BuildInstructions(object):
     """
     Used to store instructions on how to build a Docker image.
 
     TODO: only allow relative, forward roots
     """
-    def __init__(self,
-                 root: str,
-                 tag: str,
-                 context: str,
-                 filename: str,
-                 arguments: Dict[str, str],
-                 depends_on: Optional[str],
-                 source: Optional[str]
-                 ) -> None:
-        self.__root = root
-        self.__tag = tag
-        self.__context = context
-        self.__filename = filename
-        self.__arguments = {k: str(v) for (k, v) in arguments.items()}
-        self.__depends_on = depends_on
-        self.__source = source
+    root = attr.ib(type=str)
+    tag = attr.ib(type=str)
+    context = attr.ib(type=str)
+
+    """
+    The path to the Dockerfile used to build the image associated with
+    these instructions, relative to the location of the build instruction
+    file.
+    """
+    filename = attr.ib(type=str)
+
+    """
+    A dictionary of build-time arguments provided during the construction
+    of the Docker image associated with these instructions.
+    """
+    arguments = attr.ib(type=Dict[str, str],
+                        converter=converter_args)
+
+    """
+    The name of the source that provides this blueprint, if any.
+    """
+    source = attr.ib(type=Optional[str])
+
+    """
+    The name of the Docker image that the construction of the image
+    associated with these build instructions depends on. If no such
+    dependency exists, None is returned.
+    """
+    depends_on = attr.ib(type=Optional[str])
 
     @property
-    def root(self) -> str:
-        return self.__root
-
-    @property
-    def source(self) -> Optional[str]:
-        """
-        The name of the source that provides this blueprint, if any.
-        """
-        return self.__source
-
-    @property
-    def depends_on(self) -> Optional[str]:
-        """
-        The name of the Docker image that the construction of the image
-        associated with these build instructions depends on. If no such
-        dependency exists, None is returned.
-        """
-        return self.__depends_on
-
-    @property
-    def tag(self) -> str:
-        return self.__tag
-    name = tag
-
-    @property
-    def context(self) -> str:
-        return self.__context
+    def name(self) -> str:
+        return self.tag
 
     @property
     def abs_context(self) -> str:
@@ -74,16 +70,8 @@ class BuildInstructions(object):
         these instructions, relative to the location of the build instruction
         file.
         """
-        return self.__filename
+        return self.file
 
     @property
     def file_abs(self) -> str:
         return os.path.join(self.root, self.file)
-
-    @property
-    def arguments(self) -> Dict[str, str]:
-        """
-        A dictionary of build-time arguments provided during the construction
-        of the Docker image associated with these instructions.
-        """
-        return dict(self.__arguments)
