9ddc2fc96359c6e8c4ea47391c275c7db63838af
ChrisTimperley <christimperley@googlemail.com>
2019-12-04 21:47:16 -0500

    added filename property

--- bugzoo/mgr/coverage/coveragepy.py
+++ bugzoo/mgr/coverage/coveragepy.py
@@ -17,20 +17,48 @@ logger.setLevel(logging.DEBUG)
 @register_as_default(Language.PYTHON)
 @register('coverage.py')
 class CoveragePyExtractor(CoverageExtractor):
+    @attr.s(frozen=True)
     class Instructions(CoverageInstructions):
+        """
+        Attributes
+        ----------
+        filename: str
+            The absolute path of the file to which coverage information is
+            saved to by coverage.py.
+        """
+        filename = attr.ib(type=str)
+
         @staticmethod
         def from_dict(d: Dict[str, Any]) -> 'CoverageInstructions':
-            return CoveragePyExtractor.Instructions()
+            assert 'filename' in d, 'expected "filename" property'
+            filename = d['filename']
+            assert os.path.isabs(filename), 'expected absolute filename'
+            return CoveragePyExtractor.Instructions(filename)
 
         def to_dict(self) -> Dict[str, Any]:
-            return {}
+            return {'filename': self.filename}
 
     @staticmethod
     def from_instructions(installation: 'BugZoo',
                           container: Container,
                           instructions: Instructions
                           ) -> None:
-        return CoveragePyExtractor(installation, container)
+        assert isinstance(instructions, CoveragePyExtractor)
+        return CoveragePyExtractor(installation,
+                                   container,
+                                   instructions.filename)
+
+    def __init__(installation: 'BugZoo',
+                 container: Container,
+                 coverage_filename: str
+                 ) -> None:
+        super().__init__(installation, container)
+        self.__coverage_filename = coverage_filename
+
+    @property
+    def coverage_filename(self) -> str:
+        """The container filepath of the coverage.py file."""
+        return self.__coverage_filename
 
     def prepare(self) -> None:
         return
@@ -46,5 +74,6 @@ class CoveragePyExtractor(CoverageExtractor):
         bug = mgr_bug[container.bug]
 
         # attempt to read the coverage file
+        filename = self.coverage_absolute_filename
 
         raise NotImplementedError
