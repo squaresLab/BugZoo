c13648e1e22328a4772ff5de4bdb3317a35c571c
CH <cheolhun7@naver.com>
2020-02-12 16:41:41 +0900

    There are no init file. So I add it

---
+++ bugzoo/cli/controllers/__init__.py
@@ -0,0 +1,5 @@
+from .bug import BugController
+from .base import BaseController
+from .container import ContainerController
+from .source import SourceController
+from .tool import ToolController
---
+++ bugzoo/mgr/__init__.py
@@ -0,0 +1,6 @@
+from .bug import BugManager
+from .build import BuildManager
+from .container import ContainerManager
+from .file import FileManager
+from .source import SourceManager
+from .tool import ToolManager
